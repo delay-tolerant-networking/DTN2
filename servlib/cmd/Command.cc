e304915643bd4059ff06de93061929eea59a6ae3
demmer <devnull@localhost>
2004-07-12 19:26:23 -0700

    Add a tcl command interface.

---
+++ servlib/cmd/Command.cc
@@ -0,0 +1,373 @@
+#include "Command.h"
+#include "debug/Debug.h"
+#include "io/NetUtils.h"
+#include "thread/Mutex.h"
+
+/******************************************************************************
+ *
+ * CommandInterp
+ *
+ *****************************************************************************/
+// static variables
+CommandInterp* CommandInterp::instance_;
+CommandList* CommandInterp::auto_reg_ = NULL;
+
+CommandInterp::CommandInterp()
+    : Logger("/command")
+{
+    interp_ = Tcl_CreateInterp();
+    
+    lock_ = new Mutex("/command/lock");
+
+    // do auto registration of commands
+    ASSERT(auto_reg_); 
+    while (!auto_reg_->empty()) {
+        CommandModule* m = auto_reg_->front();
+        auto_reg_->pop_front();
+        reg(m);
+    }
+    
+    delete auto_reg_;
+    auto_reg_ = NULL;
+}
+
+CommandInterp::~CommandInterp()
+{
+    // the destructor isn't ever called
+    NOTREACHED;
+}
+
+int
+CommandInterp::exec_file(const char* file)
+{
+    int err;
+    ScopeLock l(lock_);
+
+    log_debug("executing command file %s", file);
+    
+    err = Tcl_EvalFile(interp_, (char*)file);
+    
+    if (err != TCL_OK) {
+        logf(LOG_ERR, "tcl error: line %d, %s",
+             interp_->errorLine, Tcl_GetStringResult(interp_));
+    }
+    
+    return err;    
+}
+
+int
+CommandInterp::exec_command(const char* command)
+{
+    int err;
+    ScopeLock l(lock_);
+
+    // tcl modifies the command string while executing it, so we need
+    // to make a copy
+    char* buf = strdup(command);
+
+    log_debug("executing command '%s'", buf);
+    
+    err = Tcl_Eval(interp_, buf);
+    
+    free(buf);
+    
+    if (err != TCL_OK) {
+        logf(LOG_ERR, "tcl error: line %d, \"%s\"",
+             interp_->errorLine, interp_->result);
+    }
+    
+    return err;
+}
+
+void
+CommandInterp::reg(CommandModule *module)
+{
+    ScopeLock l(lock_);
+    
+    module->logf(LOG_DEBUG, "command registering");
+    
+    Tcl_CreateCommand(interp_, 
+                      (char*)module->name(),
+                      CommandInterp::tcl_cmd,
+                      (ClientData)module,
+                      NULL);
+    
+    modules_.push_front(module);
+
+    module->at_reg();
+}
+
+void
+CommandInterp::auto_reg(CommandModule *module)
+{
+    // this should only be called from the static initializers, i.e.
+    // we haven't been initialized yet
+    ASSERT(instance_ == NULL);
+
+    // we need to explicitly create the auto_reg list the first time
+    // since there's no guarantee of ordering of static constructors
+    if (!auto_reg_)
+        auto_reg_ = new CommandList();
+    
+    auto_reg_->push_back(module);
+}
+
+void
+CommandInterp::reg_atexit(void(*fn)(void*), void* data)
+{
+    ScopeLock l(lock_);
+    Tcl_CreateExitHandler(fn, data);
+}
+    
+int 
+CommandInterp::tcl_cmd(ClientData client_data, Tcl_Interp* interp,
+                       int argc, CONSTTCL84 char** argv)
+{
+    CommandModule* module = (CommandModule*)client_data;
+
+    const char** const_argv = (const char**)argv;
+
+    // first check for builtin commands
+    if (module->do_builtins_ && argc > 2) {
+        const char* cmd = argv[1];
+        if (strcmp(cmd, "set") == 0) {
+            return module->cmd_set(argc, const_argv, interp);
+        }
+    }
+
+    return module->exec(argc, (const char**)argv, interp);
+}
+
+void
+CommandInterp::set_result(const char* result)
+{
+    Tcl_SetResult(interp_, (char*)result, TCL_VOLATILE);
+}
+
+void
+CommandInterp::append_result(const char* result)
+{
+    Tcl_AppendResult(interp_, (char*)result, NULL);
+}
+
+void
+CommandInterp::vresultf(const char* fmt, va_list ap, bool append)
+{
+    size_t sz = 256;
+    char buf[sz];
+    
+    size_t n = vsnprintf(buf, sz, fmt, ap);
+    
+    if (n >= sz) {
+        NOTIMPLEMENTED; // XXX/demmer handle this
+    }
+
+    if (append) {
+        Tcl_AppendResult(interp_, buf, NULL);
+    } else {
+        Tcl_SetResult(interp_, buf, TCL_VOLATILE);
+    }
+}
+
+void
+CommandInterp::resultf(const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vresultf(fmt, ap, false);
+    va_end(ap);
+}
+
+void
+CommandInterp::append_resultf(const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vresultf(fmt, ap, true);
+    va_end(ap);
+}
+
+const char*
+CommandInterp::get_result()
+{
+    return Tcl_GetStringResult(interp_);
+}
+
+/******************************************************************************
+ *
+ * CommandModule
+ *
+ *****************************************************************************/
+CommandModule::CommandModule(const char* name)
+    : name_(name), do_builtins_(true)
+{
+    logpathf("/%s", name);
+
+    // Don't do registration here. This makes the code overly obscure
+    // b/c it is not obvious who own the module that is created.
+}
+
+CommandModule::~CommandModule()
+{
+}
+
+int
+CommandModule::exec(int argc, const char** argv, Tcl_Interp* interp)
+{
+    resultf("command %s not implemented", argv[0]);
+    return TCL_ERROR;
+}
+
+void
+CommandModule::resultf(const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    CommandInterp::instance()->vresultf(fmt, ap, false);
+    va_end(ap);
+}
+
+void
+CommandModule::append_resultf(const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    CommandInterp::instance()->vresultf(fmt, ap, true);
+    va_end(ap);
+}
+
+int
+CommandModule::cmd_set(int argc, const char** args, Tcl_Interp* interp)
+{
+    ASSERT(argc >= 2);
+    ASSERT(strcmp(args[1], "set") == 0);
+    
+    // handle "set binding [value]" command
+    if (argc < 3 || argc > 4) {
+        resultf("wrong number of args: expected 3-4, got %d", argc);
+        return TCL_ERROR;
+    }
+
+    const char* var = args[2];
+    const char* val = (argc == 4) ? args[3] : 0;
+  
+    std::map<std::string, Binding*>::iterator itr;
+    itr = bindings_.find(var);
+    
+    if (itr == bindings_.end()) {
+        resultf("set: binding for %s does not exist", var);
+    }
+    
+    // set value (if any)
+    Binding* b = (*itr).second;
+
+    if (argc == 4) 
+    {
+        switch(b->type_) 
+        {
+        case BINDING_INT:
+            *(b->val_.intval_) = atoi(val);
+            break;
+            
+        case BINDING_BOOL:
+            if (strcasecmp(val, "t")    == 0 ||
+                strcasecmp(val, "true") == 0 ||
+                strcasecmp(val, "1")    == 0)
+            {
+                *(b->val_.boolval_) = true;
+            }
+            else if (strcasecmp(val, "f")     == 0 ||
+                     strcasecmp(val, "false") == 0 ||
+                     strcasecmp(val, "0")     == 0)
+            {
+                *(b->val_.boolval_) = false;
+            }
+            else
+            {
+                resultf("set: invalid value '%s' for boolean", val);
+                return TCL_ERROR;
+            }
+            break;
+            
+        case BINDING_ADDR:
+        {
+            if (gethostbyname(val, b->val_.addrval_) != 0) {
+                resultf("set: invalid value '%s' for addr", val);
+                return TCL_ERROR;
+            }
+            break;
+
+        }    
+        case BINDING_STRING:
+            b->val_.stringval_->assign(val);
+            break;
+            
+        default:
+            logf(LOG_CRIT, "unimplemented binding type %d", b->type_);
+            ASSERT(0);
+        }
+    }
+
+    switch(b->type_) 
+    {
+    case BINDING_INT:
+        resultf("%d", *(b->val_.intval_));
+        break;
+
+    case BINDING_BOOL:
+        if (*(b->val_.boolval_))
+            set_result("true");
+        else
+            set_result("false");
+        break;
+        
+    case BINDING_ADDR:
+        resultf("%s", intoa(*(b->val_.addrval_)));
+        break;
+        
+    case BINDING_STRING:
+        set_result(b->val_.stringval_->c_str());
+        break;
+        
+    default:
+        logf(LOG_CRIT, "unimplemented binding type %d", b->type_);
+        ASSERT(0);
+    }
+    
+    return 0;
+}
+
+// boilerplate code
+#define BIND_FUNCTION(_fn, _type, _typecode)                            \
+void                                                                    \
+_fn(const char* name, _type* val, _type initval)                        \
+{                                                                       \
+    *val = initval;                                                     \
+    if (bindings_.find(name) != bindings_.end())                        \
+    {                                                                   \
+        log_warn("warning, binding for %s already exists", name);       \
+    }                                                                   \
+                                                                        \
+    log_debug("creating %s binding for %s -> %p", #_type, name, val);   \
+                                                                        \
+    bindings_[name] = new Binding(_typecode, val);                      \
+}
+
+BIND_FUNCTION(CommandModule::bind_i, int, BINDING_INT);
+BIND_FUNCTION(CommandModule::bind_b, bool, BINDING_BOOL);
+BIND_FUNCTION(CommandModule::bind_addr, in_addr_t, BINDING_ADDR);
+
+void
+CommandModule::bind_s(const char* name, std::string* val,
+                      const char* initval)
+{
+    val->assign(initval);
+    
+    if (bindings_.find(name) != bindings_.end()) {
+        log_warn("warning, binding for %s already exists", name);
+    }
+
+    log_debug("creating string binding for %s -> %p", name, val);
+
+    bindings_[name] = new Binding(BINDING_STRING, val);
+}
---
+++ servlib/cmd/Command.h
@@ -0,0 +1,331 @@
+#ifndef _COMMAND_H_
+#define _COMMAND_H_
+
+#include <list> 
+#include <map>
+#include <string>
+
+#include <stdarg.h>
+#include <tcl.h>
+
+#include <netinet/in.h>
+
+/*
+ * In tcl8.4, the argv to commands is const, but not so in tcl8.3, so
+ * we need this stupid define, and force cast everything to const in
+ * the implementation of tcl_cmd.
+ */
+#ifdef CONST84
+#define CONSTTCL84 CONST84
+#else
+#define CONSTTCL84
+#endif
+
+#include <string>
+#include "debug/Debug.h"
+
+// forward decls
+class CommandInterp;
+class CommandModule;
+class CommandError;
+class Mutex;
+
+/**
+ * A list of CommandModules.
+ */
+typedef std::list<CommandModule*> CommandList;
+
+/**
+ *
+ * Command interpreter class
+ * 
+ * Command files are Tcl scripts. When a module registers itself, the
+ * interpreter binds a new command with the module name.
+ *
+ */
+class CommandInterp : public Logger {    
+public:
+    /**
+     * Return the singleton instance of config. This should only ever
+     * be called after Command::init is called to initialize the
+     * instance, hence the assertion that instance_ isn't null.
+     */
+    static CommandInterp* instance() {
+        ASSERT(instance_ != NULL);
+        return instance_;
+    }
+
+    /**
+     * Initialize the interpreter instance and register any auto
+     * registered command modules.
+     */
+    static void init() {
+        ASSERT(instance_ == NULL);
+        instance_ = new CommandInterp();
+    }
+
+    /**
+     * Read in a configuration file and execute its contents
+     * \return 0 if no error, -1 otherwise.
+     */ 
+    int exec_file(const char* file);
+
+    /**
+     * Parse a single command string.
+     *
+     * \return 0 if no error, -1 otherwise
+     */
+    int exec_command(const char* command);
+
+    /**
+     * Static callback function from Tcl to execute the commands.
+     *
+     * \param client_data Pointer to config module for which this
+     *     command was registered.
+     * \param interp Tcl interpreter
+     * \param argc Argument count.
+     * \param argv Argument values.
+     */
+    static int tcl_cmd(ClientData client_data, Tcl_Interp* interp,
+                       int argc, CONSTTCL84 char **argv);
+
+    /** 
+     * Register the command module.
+     */
+    void reg(CommandModule* module);
+
+    /**
+     * Schedule the auto-registration of a command module. This _must_
+     * be called from a static initializer, before the Command
+     * instance is actually created.
+     */
+    static void auto_reg(CommandModule* module);
+
+    /**
+     * Register a function to be called at exit.
+     */
+    void reg_atexit(void(*fn)(void*), void* data);
+
+    /**
+     * Set the TclResult string.
+     */
+    void set_result(const char* result);
+
+    /**
+     * Append the string to the TclResult
+     */
+    void append_result(const char* result);
+
+    /**
+     * Format and set the TclResult string.
+     */
+    void resultf(const char* fmt, ...) PRINTFLIKE(2, 3);
+
+    /**
+     * Format and append the TclResult string.
+     */
+    void append_resultf(const char* fmt, ...) PRINTFLIKE(2, 3);
+    
+    /**
+     * Format and set the TclResult string.
+     */
+    void vresultf(const char* fmt, va_list ap, bool append);
+    
+    /**
+     * Get the TclResult string.
+     */
+    const char* get_result();
+
+    /**
+     * Return the list of registered modules
+     */
+    const CommandList* modules() { return &modules_; }
+    
+protected:
+    /**
+     * Do all the actual initialization.
+     */
+    CommandInterp();
+
+    /**
+     * Destructor is never called (and issues an assertion).
+     */
+    ~CommandInterp();
+
+
+    Mutex* lock_;			///< Lock for command execution
+    Tcl_Interp* interp_;		///< Tcl interpreter
+
+    CommandList modules_;		///< List of registered modules
+    static CommandList* auto_reg_;	///< List of odules to auto-register
+
+    static CommandInterp* instance_;	///< Singleton instance
+};
+
+
+/** 
+ * Extend this class to provide the command hooks for a specific
+ * module. Register modules with Command::instance()->reg() or use the
+ * AutoCommandModule class.
+ */
+class CommandModule : public Logger {
+public:
+    /** 
+     * Constructor
+     *
+     *  @param name Name of the module
+     */
+    CommandModule(const char* name);
+    virtual ~CommandModule();
+    
+    /** 
+     * Override this to parse the list of arguments.
+     *
+     * @param argc Argument count 
+     * @param argv Argument values
+     * @param interp Tcl interpreter
+     *
+     * @return 0 on success, -1 on error
+     */
+    virtual int exec(int argc, const char** argv, Tcl_Interp* interp);
+
+    /**
+     * Internal handling of the "set" command.
+     *
+     * @param argc Argument count 
+     * @param argv Argument values
+     * @param interp Tcl interpreter
+     *
+     * @return 0 on success, -1 on error
+     */
+    virtual int cmd_set(int argc, const char** argv, Tcl_Interp* interp);
+
+    /** 
+     * Get the name of the module.
+     */
+    const char* name() const { return name_; }
+
+protected:
+    friend class CommandInterp;
+    
+    const char* name_;          ///< Name of the module.
+    bool do_builtins_;		///< Set to false if a module doesn't want
+                                ///< builtin commands like "set"
+
+    /**
+     * Binding type constants
+     */
+    enum type_t {
+        BINDING_INVALID = -1,
+        BINDING_INT = 1,
+        BINDING_BOOL, 
+        BINDING_STRING, 
+        BINDING_ADDR
+    };
+    
+    /** 
+     * storage types for bindings 
+     */
+    struct Binding {
+        Binding(type_t type, void* val)
+            : type_(type)
+        {
+            val_.voidval_ = val;
+        }
+        
+        /**
+         * Add a destructor declaration with no implementation since
+         * this is never called.
+         */
+        ~Binding();
+
+        type_t type_;             	///< Type of the binding
+        union {
+            void*	voidval_;
+            int*	intval_;
+            bool*	boolval_;
+            in_addr_t*	addrval_;
+            std::string* stringval_;
+        } val_; 			///< Union for value pointer
+    };
+    
+    /** 
+     * Binding list.
+     *
+     * The "set" command is automatically defined for a module. Set is
+     * used to change the value of variable which are bound using the
+     * bind_* functions defined below.
+     * 
+     */
+    std::map<std::string, Binding*> bindings_;
+   
+    /**
+     * Bind an integer to the set command
+     */
+    void bind_i(const char* name, int* val, int initval = 0);
+    
+    /**
+     * Bind a boolean to the set command
+     */
+    void bind_b(const char* name, bool* val, bool initval = false);
+    
+    /**
+     * Bind a string to the set command
+     */
+    void bind_s(const char* name, std::string* str,
+                const char* initval = 0);
+
+    /**
+     * Bind an ip addr for the set command, allowing the user to pass
+     * a hostname and/or a dotted quad style address
+     */
+    void bind_addr(const char* name, in_addr_t* addrp,
+                   in_addr_t initval = INADDR_ANY);
+
+    /**
+     * Set the TclResult string.
+     */
+    void set_result(const char* result)
+    {
+        CommandInterp::instance()->set_result(result);
+    }
+
+    /**
+     * Append the TclResult string.
+     */
+    void append_result(const char* result)
+    {
+        CommandInterp::instance()->append_result(result);
+    }
+
+    /**
+     * Format and set the TclResult string.
+     */
+    void resultf(const char* fmt, ...) PRINTFLIKE(2, 3);
+
+    /**
+     * Format and set the TclResult string.
+     */
+    void append_resultf(const char* fmt, ...) PRINTFLIKE(2, 3);
+
+    /**
+     * Callback that's issued just after the command is registered.
+     * This allows commands (particularly AutoCommandModule instances)
+     * to do any post-registration activities like binding their vars.
+     */
+    virtual void at_reg() {} 
+};
+
+/**
+ * CommandModule that auto-registers itself.
+ */
+class AutoCommandModule : public CommandModule {
+public:
+    AutoCommandModule(const char* name)
+        : CommandModule(name)
+    {
+        CommandInterp::auto_reg(this);
+    }
+};   
+
+#endif /* COMMAND_H */
