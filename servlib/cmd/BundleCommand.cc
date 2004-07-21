211015ff8b59f67d608e18d16afafac53877a062
demmer <devnull@localhost>
2004-07-21 14:17:36 -0700

    add an inject command

--- servlib/cmd/BundleCommand.cc
+++ servlib/cmd/BundleCommand.cc
@@ -1,6 +1,7 @@
 
 #include "BundleCommand.h"
 #include "bundling/Bundle.h"
+#include "bundling/BundleForwarding.h"
 
 BundleCommand BundleCommand::instance_;
 
@@ -16,6 +17,17 @@ BundleCommand::exec(int argc, const char** argv, Tcl_Interp* interp)
     }
 
     const char* cmd = argv[1];
+
+    if (strcmp(cmd, "inject") == 0) {
+        // bundle inject <source> <dest> <payload>
+        if (argc != 5) {
+            wrong_num_args(argc, argv, 2, 5, 5);
+            return TCL_ERROR;
+        }
+
+        Bundle* b = new Bundle(argv[2], argv[3], argv[4]);
+        BundleForwarding::instance()->input(b);
+    }
 
     resultf("unknown bundle subcommand %s", cmd);
     return TCL_ERROR;
