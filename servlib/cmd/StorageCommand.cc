d3ea6db0cc81e0650fa281b43278d8e44250cac7
demmer <devnull@localhost>
2004-07-22 14:05:39 -0700

    fix include since PostgresSQLStore was removed

--- servlib/cmd/StorageCommand.cc
+++ servlib/cmd/StorageCommand.cc
@@ -1,6 +1,6 @@
 
 #include "StorageCommand.h"
-#include "storage/PostgresSQLStore.h"
+#include "storage/SQLStore.h"
 #include "storage/PostgresSQLImplementation.h"
 
 StorageCommand StorageCommand::instance_;
