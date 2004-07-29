76cbc1ae21bb5e0c3f33e4ab34e3e2e6fc1ab3b7
demmer <devnull@localhost>
2004-07-28 17:16:28 -0700

    add next_bundleid and next_regid

--- servlib/storage/GlobalStore.cc
+++ servlib/storage/GlobalStore.cc
@@ -20,4 +20,39 @@ GlobalStore::serialize(SerializeAction* a)
     a->process("next_regid",    &next_regid_);
 }
 
+/**
+ * Get a new bundle id, updating the value in the store
+ *
+ * (was db_update_bundle_id, db_restore_bundle_id)
+ */
+u_int32_t
+GlobalStore::next_bundleid()
+{
+    log_debug("next_bundleid %d -> %d", next_bundleid_, next_bundleid_ + 1);
+    u_int32_t ret = next_bundleid_++;
+    if (! update()) {
+        log_err("error updating globals table");
+    }
+
+    return ret;
+}
+    
+/**
+ * Get a new unique registration id, updating the running value in
+ * the persistent table.
+ *
+ * (was db_new_regID, db_update_registration_id, db_retable_registration_id)
+ */
+u_int32_t
+GlobalStore::next_regid()
+{
+    log_debug("next_regid %d -> %d", next_regid_, next_regid_ + 1);
+    u_int32_t ret = next_regid_++;
+    if (! update()) {
+        log_err("error updating globals table");
+    }
+
+    return ret;
+}
+
 
