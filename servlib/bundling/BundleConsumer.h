258c589d47ffa8b38dee160c982aead43b2f0415
demmer <devnull@localhost>
2004-08-05 11:07:32 -0700

    Add a (thin) BundleConsumer interface that's implemented by both Contact and Registration.

---
+++ servlib/bundling/BundleConsumer.h
@@ -0,0 +1,41 @@
+#ifndef _BUNDLE_CONSUMER_H_
+#define _BUNDLE_CONSUMER_H_
+
+class Bundle;
+class BundleTuple;
+
+/**
+ * Abstract interface used to implement a "next hop" to the bundle
+ * forwarder, i.e either a local registration or a real next hop
+ * contact.
+ */
+class BundleConsumer {
+public:
+    /**
+     * Constructor.
+     */
+    BundleConsumer(const BundleTuple* dest_tuple)
+        : dest_tuple_(dest_tuple) {}
+    
+    /**
+     * Consume the given bundle, queueing it if required.
+     */
+    virtual void consume_bundle(Bundle* bundle) = 0;
+
+    /**
+     * Each BundleConsumer has a tuple (either the registration
+     * endpoint or the next-hop address).
+     */
+    const BundleTuple* dest_tuple() { return dest_tuple_; }
+    
+protected:
+    const BundleTuple* dest_tuple_;
+
+private:
+    /**
+     * Private default constructor is not used.
+     */
+    BundleConsumer();
+};
+
+#endif /* _BUNDLE_CONSUMER_H_ */
