4ae60d25f145d9f07293e2a824c7018c391239ea
demmer <devnull@localhost>
2004-08-31 18:26:58 -0700

    update to changes in Bundle constructor, allow length to be passed in explicitly

--- servlib/cmd/BundleCommand.cc
+++ servlib/cmd/BundleCommand.cc
@@ -26,13 +26,28 @@ BundleCommand::exec(int argc, const char** argv, Tcl_Interp* interp)
     const char* cmd = argv[1];
 
     if (strcmp(cmd, "inject") == 0) {
-        // bundle inject <source> <dest> <payload>
-        if (argc != 5) {
-            wrong_num_args(argc, argv, 2, 5, 5);
+        // bundle inject <source> <dest> <payload> <length?>
+        if (argc < 5 || argc > 6) {
+            wrong_num_args(argc, argv, 2, 5, 6);
             return TCL_ERROR;
         }
 
-        Bundle* b = new Bundle(argv[2], argv[3], argv[4]);
+        Bundle* b = new Bundle(argv[2], argv[3]);
+        
+        int len = strlen(argv[4]);
+        if (argc == 5) {
+            b->payload_.set_data(argv[4], len);
+        } else {
+            int total = atoi(argv[5]);
+            b->payload_.set_length(total);
+            b->payload_.append_data(argv[4], len);
+
+            total -= len;
+            for (; total > 0; --total) {
+                b->payload_.mutable_data().push_back('.');
+            }
+        }
+        
         BundleRouter::dispatch(new BundleReceivedEvent(b));
         return TCL_OK;
     } else {
