9c94c20750ad0909ae8e2977b8889ed720d90d84
demmer <devnull@localhost>
2004-08-31 18:26:04 -0700

    Track the payload length separately from the length of the in-memory string to allow for building up the payload string in chunks as well as future implementation that stores the payload on disk.

--- servlib/bundling/BundlePayload.h
+++ servlib/bundling/BundlePayload.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include "storage/Serialize.h"
+#include "debug/Debug.h"
 
 /**
  * The representation of a bundle payload.
@@ -25,30 +26,55 @@ public:
     }
     
     /**
-     * Set the payload data.
+     * Set the payload data and length.
      */
-    void set_data(u_char* bp, size_t len)
+    void set_data(const char* bp, size_t len)
     {
-        data_.assign((char*)bp, len);
+        data_.assign(bp, len);
+        length_ = len;
+    }
+
+    /**
+     * Append a chunk of payload data. Assumes that the length was
+     * previously set.
+     */
+    void append_data(const char* bp, size_t len)
+    {
+        ASSERT((data_.length() + len) <= length_);
+        data_.append(bp, len);
+    }
+
+    /**
+     * Set the payload length in preparation for filling in with data.
+     */
+    void set_length(size_t len)
+    {
+        length_ = len;
     }
     
     /**
      * The payload length.
      */
-    size_t length() const { return data_.length(); }
+    size_t length() const { return length_; }
 
     /**
      * The actual payload data.
      */
     const char* data() const { return data_.data(); }
-    
+
+    /**
+     * Accessor to the underlying data string itself.
+     */
+    std::string& mutable_data() { return data_; }
+     
     /**
      * Virtual from SerializableObject
      */
     virtual void serialize(SerializeAction* a);
 
 protected:
-    std::string data_;	/// the actual payload data
+    std::string data_;	///< the actual payload data
+    size_t length_;     ///< the payload length, potentially not all filled in
 };
 
 #endif /* _BUNDLE_PAYLOAD_H_ */
