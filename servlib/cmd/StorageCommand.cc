cb912716bebe17f05a18faaa83fc26b11f19d036
demmer <devnull@localhost>
2004-09-29 18:22:37 -0700

    misc arm related configure hacking

--- servlib/cmd/StorageCommand.cc
+++ servlib/cmd/StorageCommand.cc
@@ -5,11 +5,20 @@
 #include "storage/BerkeleyDBBundleStore.h"
 #include "storage/BerkeleyDBGlobalStore.h"
 #include "storage/BerkeleyDBRegistrationStore.h"
+
+#ifndef __SQL_DISABLED__
 #include "storage/SQLBundleStore.h"
 #include "storage/SQLGlobalStore.h"
 #include "storage/SQLRegistrationStore.h"
+#endif
+
+#ifndef __MYSQL_DISABLED__
 #include "storage/MysqlSQLImplementation.h"
+#endif
+
+#ifndef __POSTGRES_DISABLED__
 #include "storage/PostgresSQLImplementation.h"
+#endif
 
 StorageCommand StorageCommand::instance_;
 
@@ -36,7 +45,6 @@ StorageCommand::exec(int argc, const char** argv, Tcl_Interp* interp)
         return TCL_ERROR;
     }
 
-    StorageConfig* cfg = StorageConfig::instance();
     const char* cmd = argv[1];
     
     if (strcmp(cmd, "init") == 0) {
@@ -52,15 +60,19 @@ StorageCommand::exec(int argc, const char** argv, Tcl_Interp* interp)
         }
 
         const char* type = argv[2];
-        SQLImplementation* impl;
-
+        
         GlobalStore* global_store;
         BundleStore* bundle_store;
         RegistrationStore* reg_store;
-        
-        if ((strcmp(type, "mysql") == 0) ||
+
+        if (0) {
+        }
+#ifndef __SQL_DISABLED__
+        else if ((strcmp(type, "mysql") == 0) ||
             (strcmp(type, "postgres") == 0))
         {
+            StorageConfig* cfg = StorageConfig::instance();
+            SQLImplementation* impl;
             if (strcmp(type, "mysql") == 0) {
                 impl = new MysqlSQLImplementation();
             } else {
@@ -76,14 +88,20 @@ StorageCommand::exec(int argc, const char** argv, Tcl_Interp* interp)
             global_store = new SQLGlobalStore(impl);
             bundle_store = new SQLBundleStore(impl);
             reg_store    = new SQLRegistrationStore(impl);
-
-        } else if (strcmp(type, "berkeleydb") == 0) {
+        }
+#endif
+#ifndef __DB_DISABLED__
+        else if (strcmp(type, "berkeleydb") == 0)
+        {
             BerkeleyDBManager::instance()->init();
             global_store = new BerkeleyDBGlobalStore();
             bundle_store = new BerkeleyDBBundleStore();
             reg_store    = new BerkeleyDBRegistrationStore();
             
-        } else {
+        }
+#endif
+        else
+        {
             resultf("storage type %s not implemented", type);
             return TCL_ERROR;
         }
