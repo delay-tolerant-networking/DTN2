976ccb0b1ad45da522ba640a2b65ef72660515ec
demmer <devnull@localhost>
2004-09-30 12:19:58 -0700

    if tclreadline isn't available, handle it "gracefully" by just not starting an actual command line interpreter

--- servlib/cmd/command-init.tcl
+++ servlib/cmd/command-init.tcl
@@ -4,21 +4,34 @@
 # time.
 #
 
-package require tclreadline
+if [catch {
+    error "test"
+    package require tclreadline
 
-# Run the command loop with the given prompt
-proc command_loop {prompt} {
-    global command_prompt
-    set command_prompt $prompt
+    # Run the command loop with the given prompt
+    proc command_loop {prompt} {
+	global command_prompt
+	set command_prompt $prompt
 
-    namespace eval tclreadline {
-	proc prompt1 {} {
-	    global command_prompt
-	    return "${command_prompt}% "
+	namespace eval tclreadline {
+	    proc prompt1 {} {
+		global command_prompt
+		return "${command_prompt}% "
+	    }
 	}
-    }
 
-    tclreadline::Loop
+	tclreadline::Loop
+    }
+} err] {
+    log /tcl WARNING "can't load tclreadline: $err"
+    log /tcl WARNING "no command loop available"
+
+    # Set up a dummy proc to run a "command loop" that just waits
+    # forever in the tcl event loop
+    proc command_loop {prompt} {
+	global forever
+	vwait forever
+    }
 }
 
 # Define a bgerror proc to print the error stack when errors occur in
