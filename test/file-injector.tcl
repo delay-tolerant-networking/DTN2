4f2610a92c91a1b8d5c67e43f90f55b33d2180ad
demmer <devnull@localhost>
2004-09-30 11:21:07 -0700

    implement the file injector

--- test/file-injector.tcl
+++ test/file-injector.tcl
@@ -1,8 +1,83 @@
 #
-# DTN test code that scans a directory for new data and injects
+# Simple test code that scans a directory for new data and injects
 # whatever it finds as a bundle.
 #
 
-proc file_injector {dir source dest} {
-    error "file injector not implemented"
+#
+# Starts up a file injector to scan the given directory and create
+# new bundles from any files that are found.
+#
+# usage: file_injector <dir> <source> <dest> <-period ms>
+#
+proc file_injector_start {dir source dest args} {
+    global file_injector_handles
+    
+    set period 2000
+    
+    if {$args != {}} {
+	error "XXX/demmer implement optional args"
+    }
+
+    set after_handle [after 0 \
+	    [list file_injector_scan $dir $source $dest $period]]
+    set file_injector_handles($dir) $after_handle
+
+    return $dir
+}
+
+#
+# Stops a file injector on the given directory
+#
+proc file_injector_stop {dir} {
+    global file_injector_handles
+
+    if [info exists file_injector_handles($dir)] {
+	after cancel $file_injector_handles($dir)
+    } else {
+	error "no injector running on dir $dir"
+    }
+}
+
+#
+# Internal proc that actually does the scanning
+#
+proc file_injector_scan {dir source dest period} {
+    global file_injector_handles
+    
+    set files [glob -nocomplain -- "$dir/*"]
+
+    log /file_injector DEBUG "scanning $dir... found [llength $files] files"
+    foreach file $files {
+	file stat $file file_stat
+	
+	log /file_injector DEBUG "stat type $file_stat(type)"
+	
+	if {$file_stat(type) != "file"} {
+	    log /file_injector DEBUG \
+		    "ignoring file $file type $file_stat(type)"
+	    continue
+	}
+
+	if {$file_stat(size) == 0} {
+	    log /file_injector DEBUG "ignoring empty file $file"
+	    continue
+	}
+
+	log /file_injector DEBUG "reading payload from bundle file $file"
+	set fd [open $file]
+	set payload [read $fd]
+	close $fd
+
+	log /file_injector DEBUG "got [string length $payload] length payload"
+
+	bundle inject $source $dest $payload
+
+	file delete $file
+    }
+
+    # re-schedule the scan
+    set after_handle [after $period \
+	    [list file_injector_scan $dir $source $dest $period]]
+    set file_injector_handles($dir) $after_handle
+    return ""
 }
