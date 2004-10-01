082c76b50aaabe59074c6ad008599515eb556f2a
demmer <devnull@localhost>
2004-10-01 13:44:47 -0700

    pass the registration id to the tcl callback

--- test/tcl-registration.tcl
+++ test/tcl-registration.tcl
@@ -6,20 +6,25 @@
 # registration's delivery list. Loops until there are no more bundles
 # on the list, calling the delivery callback on each one
 
-proc tcl_registration_bundle_ready {regid endpoint callback} {
+proc tcl_registration_bundle_ready {regid endpoint callback callback_data} {
     while {1} {
 	set bundle_data [registration tcl $regid $endpoint get_bundle_data]
 	if {$bundle_data == {}} {
 	    return
 	}
 
-	$callback $bundle_data
+	if {$callback_data != ""} {
+	    $callback $regid $bundle_data $callback_data
+	} else {
+	    $callback $regid $bundle_data
+	}
     }
 }
-    
-proc tcl_registration {endpoint callback} {
+
+proc tcl_registration {endpoint callback {callback_data ""}} {
     set regid [registration add tcl $endpoint]
     set chan [registration tcl $regid $endpoint get_list_channel]
-    fileevent $chan readable \
-	    [list tcl_registration_bundle_ready $regid $endpoint $callback]
+    fileevent $chan readable [list tcl_registration_bundle_ready \
+	    $regid $endpoint $callback $callback_data]
+    return $regid
 }
