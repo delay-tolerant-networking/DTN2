a3bc48f348c18abfe529a3171b88dc07ca5254f5
sushjain <devnull@localhost>
2004-10-05 15:40:19 -0700

    updated creating files

--- test/mkfile.sh
+++ test/mkfile.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/csh
 
-#Usage: mkfile <size>
+#Usage: mkfile.sh <size> <name>
 #Creates empty file of given size. k for KB, m for MB. default byte.
 #Example: mkfile 1m creates a file named 1m.file of size 1 Megabytes
 
-suffix=file
+if ($#argv != 2) then
+    echo " Usage  mkfile.sh <size> <name> "
+    exit 0
+endif
 
-echo "Creating file $1.$suffix  of size $1"
-head -c $1 /dev/zero > $1.$suffix
-
+echo "Creating file $2  of size $1"
+head -c $1 /dev/zero > $2
