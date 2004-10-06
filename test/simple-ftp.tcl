4c65dee90af6f4b2ce4f5a0deca800e7948caecb
sushjain <devnull@localhost>
2004-10-05 20:52:22 -0700

    logging in simple ftp

--- test/simple-ftp.tcl
+++ test/simple-ftp.tcl
@@ -1,4 +1,13 @@
 
+#
+# Simple client/server code that scans a directory for new data and sends
+# that data to a remote host.
+#
+# usage: simple_ftp server <dir> <logfile>
+# usage: simple_ftp client <dir> <logfile> <host>
+#
+#
+
 set port 17600
 set period 1000
 
@@ -30,11 +39,14 @@ proc scan_dir {host dir} {
 
 proc send_file {host file} {
     global port
+    global $logfd
     set fd [open $file]
     set payload [read $fd]
     close $fd
 
     puts "sending file $file"
+    puts $logfd "[clock format [clock seconds]] :: got file $file " 
+    flush $logfd
     set sock [socket $host $port]
     puts $sock "[file tail $file]"
     puts -nonewline $sock $payload
@@ -49,11 +61,14 @@ proc recv_files {dest_dir} {
 }
 
 proc file_arrived {dest_dir sock addr port} {
+    global logfd
     set file [gets $sock]
     set payload [read $sock]
     close $sock
 
     puts "got file $file"
+    puts $logfd "[clock format [clock seconds]] :: got file $file " 
+    flush $logfd
     set fd [open "$dest_dir/$file" w]
     puts -nonewline $fd $payload
     close $fd
@@ -61,15 +76,18 @@ proc file_arrived {dest_dir sock addr port} {
 
 set mode [lindex $argv 0]
 set dir  [lindex $argv 1]
+set logfile  [lindex $argv 2]
 
 if {$mode == "server"} {
     recv_files $dir
 } elseif {$mode == "client"} {
-    set host [lindex $argv 2]
+    set host [lindex $argv 3]
     scan_dir $host $dir
 } else {
     error "unknown mode $mode"
 }
+set logfd [open $logfile w]
+puts $logfd "[clock format [clock seconds]] :: Starting in $mode " 
 
 
 vwait forever
