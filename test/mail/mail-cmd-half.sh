87f9fec0a5cd48f072fe355eb1379b7050be113b
rabin <devnull@localhost>
2004-10-14 14:52:38 -0700

    addingi host status timeout TO_HOSTSTATUS -changing itto 20s from 30m so that disconnection can be dtected earlier

--- test/mail/mail-cmd-half.sh
+++ test/mail/mail-cmd-half.sh
@@ -29,7 +29,7 @@ endif
 set queuename = q_$proto
 set exp_type = 1 
 set mailqueuedir = /var/spool/$queuename/
-
+set hoststatus_timout = 20s
 if($exp_type == 1) then
     set delivery_mode = b
     set queueruntime  = 1s
@@ -58,6 +58,7 @@ sed -i "s/__LOCAL_DOMAIN__/$local_mail_domain/g" $logroot/sendmail-$id.mc  >>& $
 sed -i "s/__DELIVERY_MODE__/$delivery_mode/g" $logroot/sendmail-$id.mc  >>& $info
 sed -i "s/__SINGLE_THREAD__/$singlethread/g" $logroot/sendmail-$id.mc  >>& $info
 sed -i "s/__HOST_STATUS_DIRECTORY__/$host_stat_dir/g" $logroot/sendmail-$id.mc  >>& $info
+sed -i "s/__HOSTSTATUS_TIMEOUT__/$host_stat_dir/g" $logroot/sendmail-$id.mc  >>& $info
 sed -i "s/__CACHE_SIZE__/$cache_size/g" $logroot/sendmail-$id.mc  >>& $info
 sed -i "s/__QUEUE_DIR__/$queuename/g" $logroot/sendmail-$id.mc  >>& $info
 
