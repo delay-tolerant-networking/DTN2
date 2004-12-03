f6c5d7963b6741bf8ada8a7aa83260ffe7b19614
demmer <devnull@localhost>
2004-12-02 20:04:12 -0800

    add a tcl registration

--- test/send-one-bundle.tcl
+++ test/send-one-bundle.tcl
@@ -12,8 +12,18 @@ test set initscript {
     log "/test" INFO "route dump:\n[route dump]"
     
     if {$id == 0} {
-	
+	proc test_arrived {regid bundle_info} {
+	    foreach {source dest payload length} $bundle_info {}
+	    puts "bundle arrival"
+	    puts "source:  $source"
+	    puts "dest:    $dest"
+	    puts "length:  $length"
+	    puts "payload: [string range $payload 0 64]"
+	}
+
+	tcl_registration "bundles://*/*" test_arrived
     }
+    
     if {$id == 1} {
 	sendbundle
     }
