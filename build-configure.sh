fcc94b1632a8b3d9b40dc16d695c39eebf371e4b
demmer <devnull@localhost>
2005-02-11 16:31:27 -0800

    oy -- change the string in the echo lines to "build-configure" not "preconfig"

--- build-configure.sh
+++ build-configure.sh
@@ -9,33 +9,33 @@
 
 trap 'rm -f aclocal.m4 ; exit 0' 0 1 2 3 13 15
 
-echo "preconfig: building aclocal.m4..."
+echo "build-configure: building aclocal.m4..."
 rm -f aclocal.m4
 cat aclocal/*.ac > aclocal.m4
 
 if [ -d oasys ] ; then
-    echo "preconfig: loading oasys functions into aclocal.m4..."
+    echo "build-configure: loading oasys functions into aclocal.m4..."
     cat oasys/aclocal/*.ac >> aclocal.m4
 else
-    echo "preconfig: ERROR -- can't find oasys for autoconf macros"
+    echo "build-configure: ERROR -- can't find oasys for autoconf macros"
     exit 1
 fi
 
-echo "preconfig: running autoscan to find missing checks..."
+echo "build-configure: running autoscan to find missing checks..."
 autoscan
 
-echo "preconfig: running autoheader to build config.h.in..."
+echo "build-configure: running autoheader to build config.h.in..."
 rm -f auto.config.h auto.config.h.in
 autoheader
 chmod 444 auto.config.h.in
 
-echo "preconfig: running autoconf to build configure..."
+echo "build-configure: running autoconf to build configure..."
 rm -f configure
 autoconf
 chmod 555 configure
 
-echo "preconfig: purging configure cache..."
+echo "build-configure: purging configure cache..."
 rm -rf autom4te.cache
 rm -f config.cache
 
-echo "preconfig: done."
+echo "build-configure: done."
