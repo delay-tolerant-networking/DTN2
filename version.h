6e5ad3c4105397f5725f176bf5d9889d13537b3f
demmer <devnull@localhost>
2005-03-08 18:55:27 -0800

    bump version number

--- version.h
+++ version.h
@@ -2,9 +2,9 @@
 #define _DTN_VERSION_H_
 
 #define DTN_VERSION_MAJOR	2
-#define DTN_VERSION_MINOR	0
-#define DTN_VERSION_PATCH	1
-#define DTN_VERSION_STRING	"DTN Reference Implementation version 2.0.1"
+#define DTN_VERSION_MINOR	1
+#define DTN_VERSION_PATCH	0
+#define DTN_VERSION_STRING	"DTN Reference Implementation version 2.1.0"
 
 extern const char* dtn_version;
 
