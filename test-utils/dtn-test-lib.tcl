a85d49984a83f707e959f1e80c09fc7e94b4cb66
demmer <devnull@localhost>
2005-09-22 14:22:11 -0700

    if not running in an xterm, pass the -d argument to dtnd

--- test-utils/dtn-test-lib.tcl
+++ test-utils/dtn-test-lib.tcl
@@ -10,12 +10,16 @@ manifest::dir  db
 
 # options procedure for running dtnd
 proc dtnd_opts {id} {
-    global net::host net::portbase net::extra test::testname
+    global opt net::host net::portbase net::extra test::testname
 
     set dtnd(exec_file) dtnd
     set dtnd(exec_opts) "-i $id -t -c $test::testname.conf"
     set dtnd(confname)  $test::testname.conf
     set dtnd(conf)      [conf::get dtnd $id]
+
+    if {! $opt(xterm)} {
+	append dtnd(exec_opts) " -d"
+    }
 
     # XXX/demmer add config hook for console (not just cmdline arg)
     append dtnd(exec_opts) " --console-port [dtn_port console $id]"
