78b949b1e337aeb22d249f7590764ea4a8ecb2c7
podolsky <devnull@localhost>
2005-10-04 19:06:39 -0700

    Update test to check for the bundle's arrival based on its GUID (source EID + timestamp)

--- test/send-one-bundle.tcl
+++ test/send-one-bundle.tcl
@@ -7,20 +7,37 @@ dtn::config_interface tcp
 dtn::config_linear_topology ONDEMAND tcp true
 
 conf::add dtnd 0 {
-    proc test_arrived {regid bundle_info} {
-	array set b $bundle_info
-	if ($b(isadmin)) {
-	    error "Unexpected admin bundle arrival $b(source) -> b($dest)"
-	}
-	puts "bundle arrival"
-	foreach {key val} [array get b] {
-	    if {$key == "payload"} {
-		puts "payload:\t [string range $b(payload) 0 64]"
-	    } else {
-		puts "$key:\t $b($key)"
-	    }
-	}
+    
+    proc test_arrived {regid bundle_data} {
+        array set b $bundle_data
+        if ($b(isadmin)) {
+            error "Unexpected admin bundle arrival $b(source) -> b($dest)"
+        }
+        puts "bundle arrival"
+        foreach {key val} [array get b] {
+            if {$key == "payload"} {
+                puts "payload:\t [string range $b(payload) 0 64]"
+            } else {
+                puts "$key:\t $b($key)"
+            }
+        }
+        # record the bundle's arrival
+        set guid "$b(source),$b(creation_ts)"
+        global bundle_payloads
+        set bundle_payloads($guid) $b(payload)
+        global bundle_info
+        unset b(payload)
+        set bundle_info($guid) [array get b]
     }
+
+    proc check_bundle_arrived {bundle_guid} {
+        global bundle_info
+        if {[info exists bundle_info($bundle_guid)]} {
+            return true
+        }
+        return false
+    }
+    
 }
 
 test::script {
@@ -31,23 +48,34 @@ test::script {
     dtn::wait_for_dtnd *
     
     dtn::tell_dtnd 0 {
-	tcl_registration dtn://host-0/test test_arrived
+        tcl_registration dtn://host-0/test test_arrived
     }
 
-    puts "* sending bundle"
     set last_node [expr [net::num_nodes] - 1]
-    dtn::tell_dtnd $last_node {
-	sendbundle [route local_eid] dtn://host-0/test
-    }
+    set source [dtn::tell_dtnd $last_node {
+        route local_eid
+    }]
+    
+    puts "* sending bundle"
+    set timestamp [dtn::tell_dtnd $last_node {
+        sendbundle [route local_eid] dtn://host-0/test
+    }]
 
     puts "* waiting for bundle arrival"
-    do_until "checking for bundle arrival" 5000 {
-	set stats [dtn::tell_dtnd 0 "bundle stats"]
-	if {[string match "* 1 received *" $stats]} {
-	    break
-	}
-    }
+    do_until "checking for bundle arrival" 5000 [subst -nocommand {
+        if {[dtn::tell_dtnd 0 {check_bundle_arrived "$source,$timestamp"}]} {
+            break
+        }
+    }]
 
+    puts "* doing sanity check on stats"
+    do_until "stats sanity check" 5000 {
+        set stats [dtn::tell_dtnd 0 "bundle stats"]
+        if {[string match "* 1 received *" $stats]} {
+            break
+        }
+    }
+    
     puts "* test success!"
 }
 
