4b07535ca73b64ca51dcc7dddfccb37a2d09f17d
demmer <devnull@localhost>
2005-10-17 14:29:55 -0700

    add a hook so the test tries both a clean shutdown and tests the test script handling to explicitly kill the spawned proc

--- test/startstop.tcl
+++ test/startstop.tcl
@@ -8,13 +8,21 @@ dtn::config_linear_topology ONDEMAND tcp true
 test::script {
     puts "* Startstop test script executing..."
     dtn::run_dtnd *
+    
     puts "* Waiting for dtnd"
     dtn::wait_for_dtnd *
     dtn::tell_dtnd * {puts "startstop test"}
-    puts "* Startstop test script complete"
+
+    puts "* Shutting down dtnd"
+    dtn::tell_dtnd * {shutdown}
+
+    puts "* Waiting for dtnd to quit"
+    run::wait_for_programs
+
+    puts "* Running new dtnds"
+    dtn::run_dtnd *
 }
 
 test::exit_script {
-    puts "* Exiting dtn daemons"
-    dtn::tell_dtnd * {exit}
+    puts "* Leaving dtn daemons running for the test utils to kill them..."
 }
