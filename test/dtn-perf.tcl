5c19d9dcc174d84494e7d5df8489f86f45a56be9
demmer <devnull@localhost>
2005-10-21 16:54:36 -0700

    add option for file based payloads

--- test/dtn-perf.tcl
+++ test/dtn-perf.tcl
@@ -9,19 +9,29 @@ dtn::config
 dtn::config_interface tcp
 dtn::config_linear_topology ONDEMAND tcp true
 
-global perftime delivery_opts
 set perftime 60
 set delivery_opts ""
 
-foreach {var val} $opt(opts) {
+set mode "memory"
+
+for {set i 0} {$i < [llength $opt(opts)]} {incr i} {
+    set var [lindex $opt(opts) $i]
+
     if {$var == "-perftime" } {
-	set perftime $val
+	set perftime [lindex $opt(opts) [incr i]]
 
     } elseif {$var == "-forwarding_rcpts" } {
 	append delivery_opts "-F "
 
     } elseif {$var == "-receive_rcpts" } {
 	append delivery_opts "-R "
+
+    } elseif {$var == "-payload_len"} {
+	set len [lindex $opt(opts) [incr i]]
+	append delivery_opts "-p $len "
+
+    } elseif {$var == "-file_payload"} {
+	set mode "file"
 	
     } else {
 	puts "ERROR: unrecognized test option '$var'"
@@ -29,9 +39,13 @@ foreach {var val} $opt(opts) {
     }
 }
 
-test::script {
-    global perftime
+if {$mode == "memory"} {
+    append delivery_opts "-m "
+} else {
+    append delivery_opts "-f dtnperf.snd "
+}
 
+test::script {
     puts "* Running dtnds"
     dtn::run_dtnd *
 
@@ -42,16 +56,20 @@ test::script {
     set last_node [expr $N - 1]
     set dest      dtn://host-0
 
-    # XXX hacky -d /tmp specification because dtnperf-server is too
-    # braindead to not care about creating the directory even when
-    # bundles are saved into memory (-m), and I don't feel like
-    # touching that code
-    set server_pid [dtn::run_app 0 dtnperf-server "-m -v -a 100 -d /tmp"]
+    # make sure we properly tell the client and server where to put things
+    set rundir [dist::get_rundir $net::host($last_node) $last_node]
+    regsub {dtnperf.snd} $delivery_opts "$rundir/dtnperf.snd" delivery_opts
+    
+    set server_opts "-v -a 100 -d $rundir "
+    if {$mode == "memory"} {
+	append server_opts "-m"
+    }
+    set server_pid [dtn::run_app 0 dtnperf-server $server_opts]
     after 1000
-
+    
     puts "* Running dtnperf-client for $perftime seconds"
     set client_pid [dtn::run_app $last_node dtnperf-client \
-			"-t $perftime -m $delivery_opts -d $dest" ]
+			"-t $perftime $delivery_opts -d $dest" ]
 
     # XXX might want to try running dtnperf-client when sending to a
     # non-existent endpoint too, such as:
@@ -64,7 +82,8 @@ test::script {
 	puts ""
 	after 1000
     }
-    
+
+    puts "* waiting for dtnperf-client to exit"
     run::wait_for_pid_exit $last_node $client_pid
 
     puts "* Final stats:"
