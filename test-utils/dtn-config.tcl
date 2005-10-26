f020ff968588f89ea02b924f3c7d306a95aaa03c
demmer <devnull@localhost>
2005-10-26 09:47:45 -0700

    * also resolve hostnames when setting the interface listening address

--- test-utils/dtn-config.tcl
+++ test-utils/dtn-config.tcl
@@ -50,7 +50,7 @@ proc config_interface {cl args} {
 	set port [dtn::get_port $cl $id]
 	
 	conf::add dtnd $id [eval list interface add ${cl}0 $cl \
-		local_addr=$host local_port=$port $args]
+		local_addr=[gethostbyname $host] local_port=$port $args]
     }
 }
 
