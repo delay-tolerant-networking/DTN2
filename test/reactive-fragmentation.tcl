450b4021759d4b61239af6bc9becf45ad58d5b0f
demmer <devnull@localhost>
2006-05-15 09:58:11 -0700

    * fix up the test to have shorter intervals between killing links and   more times when the link dies to try to catch more cases requiring   reassembly * check that no bundles are pending when the test is through

--- test/reactive-fragmentation.tcl
+++ test/reactive-fragmentation.tcl
@@ -23,8 +23,8 @@ test::script {
     set timestamp [dtn::tell_dtnd 1 sendbundle \
 	    dtn://host-1/test dtn://host-0/test length=8096]
 
-    for {set i 0} {$i < 3} {incr i} {
-	set wait [expr 3000 + int(5000 * rand())]
+    for {set i 0} {$i < 5} {incr i} {
+	set wait [expr int(5000 * rand())]
 	puts "* Waiting [expr $wait / 1000] seconds"
 	after $wait
 	
@@ -38,7 +38,9 @@ test::script {
     puts "* Waiting for the bundle to arrive and to be reassembled"
     dtn::wait_for_bundle 0 "dtn://host-1/test,$timestamp" 30000
 
-    # XXX/demmer test if 
+    puts "* Checking that no bundles are pending"
+    dtn::wait_for_bundle_stats 0 {0 pending 1 delivered}
+    dtn::wait_for_bundle_stats 1 {0 pending}
     
     puts "* Test success!"
 }
