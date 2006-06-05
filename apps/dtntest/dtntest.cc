b7dd9d9ba79ea0d21c050c8db299df3ac664d49a
demmer <devnull@localhost>
2006-06-05 14:43:42 -0700

    * add implementations of most of the rest of the api

--- apps/dtntest/dtntest.cc
+++ apps/dtntest/dtntest.cc
@@ -41,7 +41,10 @@
 #include <oasys/tclcmd/ConsoleCommand.h>
 #include <oasys/tclcmd/TclCommand.h>
 #include <oasys/util/Getopt.h>
+#include <oasys/util/OptParser.h>
+
 #include <dtn_api.h>
+#include <EndpointIDOpt.h>
 
 typedef std::map<int, dtn_handle_t> HandleMap;
 
@@ -113,6 +116,449 @@ public:
 };
 
 //----------------------------------------------------------------------
+oasys::EnumOpt::Case FailureActionCases[] = {
+    {"drop",  DTN_REG_DROP},
+    {"defer", DTN_REG_DEFER},
+    {"exec",  DTN_REG_EXEC},
+    {0, 0}
+};
+
+class DTNRegisterCommand : public oasys::TclCommand {
+public:
+    oasys::OptParser parser_;
+
+    struct RegistrationOpts {
+        dtn_endpoint_id_t endpoint_;
+        int               failure_action_;
+        u_int             expiration_;
+        std::string       script_;
+    };
+    
+    RegistrationOpts opts_;
+
+    void init_opts() {
+        memset(&opts_.endpoint_, 0, sizeof(opts_.endpoint_));
+        opts_.failure_action_ = DTN_REG_DROP;
+        opts_.expiration_ = 0;
+        opts_.script_ = "";
+    }
+
+    DTNRegisterCommand() : TclCommand("dtn_register")
+    {
+        parser_.addopt(new dtn::EndpointIDOpt("endpoint", &opts_.endpoint_));
+        parser_.addopt(new oasys::EnumOpt("failure_action",
+                                          FailureActionCases,
+                                          &opts_.failure_action_));
+        parser_.addopt(new oasys::UIntOpt("expiration", &opts_.expiration_));
+        parser_.addopt(new oasys::StringOpt("script", &opts_.script_));
+    }
+    
+    int exec(int argc, const char **argv,  Tcl_Interp* interp)
+    {
+        (void)argc;
+        (void)argv;
+        (void)interp;
+
+        // need at least cmd, handle, endpoint, and expiration
+        if (argc < 4) {
+            wrong_num_args(argc, argv, 1, 4, INT_MAX);
+            return TCL_ERROR;
+        }
+
+        int n = atoi(argv[1]);
+        HandleMap::iterator iter = State::instance()->handles_.find(n);
+        if (iter == State::instance()->handles_.end()) {
+            resultf("invalid dtn handle %d", n);
+            return TCL_ERROR;
+        }
+
+        dtn_handle_t h = iter->second;
+
+        init_opts();
+        const char* invalid = 0;
+        if (! parser_.parse(argc - 2, argv + 2, &invalid)) {
+            resultf("invalid option '%s'", invalid);
+            return TCL_ERROR;
+        }
+
+        if (opts_.endpoint_.uri[0] == 0) {
+            resultf("must set endpoint id");
+            return TCL_ERROR;
+        }
+        
+        if (opts_.expiration_ == 0) {
+            resultf("must set expiration");
+            return TCL_ERROR;
+        }
+        
+        dtn_reg_info_t reginfo;
+        memset(&reginfo, 0, sizeof(reginfo));
+
+        dtn_copy_eid(&reginfo.endpoint, &opts_.endpoint_);
+        reginfo.failure_action =
+            (dtn_reg_failure_action_t)opts_.failure_action_;
+        reginfo.expiration = opts_.expiration_;
+        reginfo.script.script_len = opts_.script_.length();
+        reginfo.script.script_val = (char*)opts_.script_.c_str();
+
+        dtn_reg_id_t regid = 0;
+        
+        int ret = dtn_register(h, &reginfo, &regid);
+        if (ret != DTN_SUCCESS) {
+            resultf("error in dtn_register: %s",
+                    dtn_strerror(dtn_errno(h)));
+            return TCL_ERROR;
+        }
+
+        resultf("%u", regid);
+        return TCL_OK;
+    }
+};
+
+//----------------------------------------------------------------------
+class DTNUnregisterCommand : public oasys::TclCommand {
+public:
+    DTNUnregisterCommand() : TclCommand("dtn_unregister") {}
+    int exec(int argc, const char **argv,  Tcl_Interp* interp)
+    {
+        (void)interp;
+
+        if (argc != 3) {
+            wrong_num_args(argc, argv, 1, 3, 3);
+            return TCL_ERROR;
+        }
+
+        int n = atoi(argv[1]);
+        HandleMap::iterator iter = State::instance()->handles_.find(n);
+        if (iter == State::instance()->handles_.end()) {
+            resultf("invalid dtn handle %d", n);
+            return TCL_ERROR;
+        }
+
+        dtn_handle_t h = iter->second;
+
+        dtn_reg_id_t regid = atoi(argv[2]);
+
+        int err = dtn_unregister(h, regid);
+        if (err != DTN_SUCCESS) {
+            resultf("error in dtn_unregister: %s",
+                    dtn_strerror(dtn_errno(h)));
+            return TCL_ERROR;
+        }
+        
+        return TCL_OK;
+    }
+};
+
+//----------------------------------------------------------------------
+oasys::EnumOpt::Case PriorityCases[] = {
+    {"bulk",      COS_BULK},
+    {"normal",    COS_NORMAL},
+    {"expedited", COS_EXPEDITED},
+    {0, 0}
+};
+
+class DTNSendCommand : public oasys::TclCommand {
+public:
+    struct SendOpts {
+        dtn_endpoint_id_t source_;
+        dtn_endpoint_id_t dest_;
+        dtn_endpoint_id_t replyto_;
+        int    priority_;
+        bool   custody_xfer_;
+        bool   receive_rcpt_;
+        bool   custody_rcpt_;
+        bool   forward_rcpt_;
+        bool   delivery_rcpt_;
+        bool   deletion_rcpt_;
+        u_int  expiration_;
+        char   payload_data_[DTN_MAX_BUNDLE_MEM];
+        size_t payload_data_len_;
+        char   payload_file_[DTN_MAX_PATH_LEN];
+        size_t payload_file_len_;
+    };
+
+    // we use a single parser and options struct for the command for
+    // improved efficiency
+    oasys::OptParser parser_;
+    SendOpts opts_;
+
+    void init_opts() {
+        memset(&opts_, 0, sizeof(opts_));
+        opts_.expiration_ = 5 * 60;
+    }
+
+    DTNSendCommand() : TclCommand("dtn_send")
+    {
+        parser_.addopt(new dtn::EndpointIDOpt("source", &opts_.source_));
+        parser_.addopt(new dtn::EndpointIDOpt("dest", &opts_.dest_));
+        parser_.addopt(new dtn::EndpointIDOpt("replyto", &opts_.replyto_));
+        parser_.addopt(new oasys::EnumOpt("priority", PriorityCases,
+                                          &opts_.priority_));
+        parser_.addopt(new oasys::BoolOpt("custody_xfer",
+                                          &opts_.custody_xfer_));
+        parser_.addopt(new oasys::BoolOpt("receive_rcpt",
+                                          &opts_.receive_rcpt_));
+        parser_.addopt(new oasys::BoolOpt("custody_rcpt",
+                                          &opts_.custody_rcpt_));
+        parser_.addopt(new oasys::BoolOpt("forward_rcpt",
+                                          &opts_.forward_rcpt_));
+        parser_.addopt(new oasys::BoolOpt("delivery_rcpt",
+                                          &opts_.delivery_rcpt_));
+        parser_.addopt(new oasys::BoolOpt("deletion_rcpt",
+                                          &opts_.deletion_rcpt_));
+        parser_.addopt(new oasys::UIntOpt("expiration",
+                                          &opts_.expiration_));
+        parser_.addopt(new oasys::CharBufOpt("payload_data",
+                                             opts_.payload_data_,
+                                             &opts_.payload_data_len_,
+                                             sizeof(opts_.payload_data_)));
+        parser_.addopt(new oasys::CharBufOpt("payload_file",
+                                             opts_.payload_file_,
+                                             &opts_.payload_file_len_,
+                                             sizeof(opts_.payload_file_)));
+    }
+    
+    int exec(int argc, const char **argv,  Tcl_Interp* interp)
+    {
+        (void)argc;
+        (void)argv;
+        (void)interp;
+
+        // need at least the command, handle, source, dest, and payload
+        if (argc < 5) {
+            wrong_num_args(argc, argv, 1, 5, INT_MAX);
+            return TCL_ERROR;
+        }
+        
+        int n = atoi(argv[1]);
+        HandleMap::iterator iter = State::instance()->handles_.find(n);
+        if (iter == State::instance()->handles_.end()) {
+            resultf("invalid dtn handle %d", n);
+            return TCL_ERROR;
+        }
+        
+        dtn_handle_t h = iter->second;
+        
+        // now parse all the options
+        init_opts();
+        const char* invalid = 0;
+        if (! parser_.parse(argc - 2, argv + 2, &invalid)) {
+            resultf("invalid option '%s'", invalid);
+            return TCL_ERROR;
+        }
+
+        // validate that source, dest, and some payload was specified
+        if (opts_.source_.uri[0] == 0) {
+            resultf("must set source endpoint id");
+            return TCL_ERROR;
+        }
+        if (opts_.dest_.uri[0] == 0) {
+            resultf("must set dest endpoint id");
+            return TCL_ERROR;
+        }
+        if (opts_.payload_data_len_ == 0 && opts_.payload_file_len_ == 0) {
+            resultf("must set payload");
+            return TCL_ERROR;
+        }
+
+        dtn_bundle_spec_t spec;
+        memset(&spec, 0, sizeof(spec));
+        dtn_copy_eid(&spec.source, &opts_.source_);
+        dtn_copy_eid(&spec.dest,   &opts_.dest_);
+        if (opts_.replyto_.uri[0] != 0) {
+            dtn_copy_eid(&spec.replyto, &opts_.replyto_);
+        }
+        spec.priority = (dtn_bundle_priority_t)opts_.priority_;
+        if (opts_.custody_xfer_)  spec.dopts |= DOPTS_CUSTODY;
+        if (opts_.receive_rcpt_)  spec.dopts |= DOPTS_RECEIVE_RCPT;
+        if (opts_.custody_rcpt_)  spec.dopts |= DOPTS_CUSTODY_RCPT;
+        if (opts_.forward_rcpt_)  spec.dopts |= DOPTS_FORWARD_RCPT;
+        if (opts_.delivery_rcpt_) spec.dopts |= DOPTS_DELIVERY_RCPT;
+        if (opts_.deletion_rcpt_) spec.dopts |= DOPTS_DELETE_RCPT;
+        spec.expiration = opts_.expiration_;
+
+        dtn_bundle_payload_t payload;
+        memset(&payload, 0, sizeof(payload));
+        if (opts_.payload_data_len_ != 0) {
+            dtn_set_payload(&payload, DTN_PAYLOAD_MEM,
+                            opts_.payload_data_, opts_.payload_data_len_);
+        } else {
+            dtn_set_payload(&payload, DTN_PAYLOAD_FILE,
+                            opts_.payload_file_, opts_.payload_file_len_);
+
+        }
+
+        dtn_bundle_id_t id;
+        memset(&id, 0, sizeof(id));
+        
+        int ret = dtn_send(h, &spec, &payload, &id);
+        if (ret != DTN_SUCCESS) {
+            resultf("error in dtn_send: %s",
+                    dtn_strerror(dtn_errno(h)));
+            return TCL_ERROR;
+        }
+
+        resultf("%s,%u.%u",
+                id.source.uri, id.creation_secs, id.creation_subsecs);
+        return TCL_OK;
+    }
+};
+
+//----------------------------------------------------------------------
+class DTNBindCommand : public oasys::TclCommand {
+public:
+    DTNBindCommand() : TclCommand("dtn_bind") {}
+    
+    int exec(int argc, const char **argv,  Tcl_Interp* interp)
+    {
+        (void)interp;
+
+        if (argc != 3) {
+            wrong_num_args(argc, argv, 1, 3, 3);
+            return TCL_ERROR;
+        }
+
+        int n = atoi(argv[1]);
+        HandleMap::iterator iter = State::instance()->handles_.find(n);
+        if (iter == State::instance()->handles_.end()) {
+            resultf("invalid dtn handle %d", n);
+            return TCL_ERROR;
+        }
+
+        dtn_handle_t h = iter->second;
+
+        dtn_reg_id_t regid = atoi(argv[2]);
+
+        int err = dtn_bind(h, regid);
+        if (err != DTN_SUCCESS) {
+            resultf("error in dtn_bind: %s",
+                    dtn_strerror(dtn_errno(h)));
+            return TCL_ERROR;
+        }
+        
+        return TCL_OK;
+    }
+};
+
+//----------------------------------------------------------------------
+class DTNUnbindCommand : public oasys::TclCommand {
+public:
+    DTNUnbindCommand() : TclCommand("dtn_unbind") {}
+    
+    int exec(int argc, const char **argv,  Tcl_Interp* interp)
+    {
+        (void)interp;
+
+        if (argc != 3) {
+            wrong_num_args(argc, argv, 1, 3, 3);
+            return TCL_ERROR;
+        }
+
+        int n = atoi(argv[1]);
+        HandleMap::iterator iter = State::instance()->handles_.find(n);
+        if (iter == State::instance()->handles_.end()) {
+            resultf("invalid dtn handle %d", n);
+            return TCL_ERROR;
+        }
+
+        dtn_handle_t h = iter->second;
+
+        dtn_reg_id_t regid = atoi(argv[2]);
+
+        int err = dtn_unbind(h, regid);
+        if (err != DTN_SUCCESS) {
+            resultf("error in dtn_unbind: %s",
+                    dtn_strerror(dtn_errno(h)));
+            return TCL_ERROR;
+        }
+        
+        return TCL_OK;
+    }
+};
+
+//----------------------------------------------------------------------
+class DTNRecvCommand : public oasys::TclCommand {
+public:
+    oasys::OptParser parser_;
+
+    struct RecvOpts {
+        bool   payload_mem_;
+        bool   payload_file_;
+        u_int  timeout_;
+    };
+    
+    RecvOpts opts_;
+
+    void init_opts() {
+        memset(&opts_, 0, sizeof(opts_));
+    }
+
+    DTNRecvCommand() : TclCommand("dtn_recv")
+    {
+        parser_.addopt(new oasys::BoolOpt("payload_mem", &opts_.payload_mem_));
+        parser_.addopt(new oasys::BoolOpt("payload_file", &opts_.payload_file_));
+        parser_.addopt(new oasys::UIntOpt("timeout", &opts_.timeout_));
+    }
+    
+    int exec(int argc, const char **argv,  Tcl_Interp* interp)
+    {
+        (void)argc;
+        (void)argv;
+        (void)interp;
+
+        // need at least cmd, handle, payload, and timeout
+        if (argc < 3) {
+            wrong_num_args(argc, argv, 1, 3, INT_MAX);
+            return TCL_ERROR;
+        }
+
+        int n = atoi(argv[1]);
+        HandleMap::iterator iter = State::instance()->handles_.find(n);
+        if (iter == State::instance()->handles_.end()) {
+            resultf("invalid dtn handle %d", n);
+            return TCL_ERROR;
+        }
+
+        dtn_handle_t h = iter->second;
+
+        init_opts();
+
+        const char* invalid = 0;
+        if (! parser_.parse(argc - 2, argv + 2, &invalid)) {
+            resultf("invalid option '%s'", invalid);
+            return TCL_ERROR;
+        }
+
+        if (opts_.payload_mem_ == false && opts_.payload_file_ == false) {
+            resultf("must set payload location");
+            return TCL_ERROR;
+        }
+
+        dtn_bundle_spec_t spec;
+        memset(&spec, 0, sizeof(spec));
+
+        dtn_bundle_payload_t payload;
+        memset(&payload, 0, sizeof(payload));
+        
+        int err = dtn_recv(h, &spec,
+                           opts_.payload_mem_ ? DTN_PAYLOAD_MEM : DTN_PAYLOAD_FILE,
+                           &payload, opts_.timeout_);
+        if (err != DTN_SUCCESS) {
+            resultf("error in dtn_recv: %s",
+                    dtn_strerror(dtn_errno(h)));
+            return TCL_ERROR;
+        }
+        
+        dtn_free_payload(&payload);
+        
+        // XXX/demmer should fill in the return with something useful, no?
+        
+        return TCL_OK;
+    }
+};
+
+//----------------------------------------------------------------------
 class ShutdownCommand : public oasys::TclCommand {
 public:
     ShutdownCommand() : TclCommand("shutdown") {}
@@ -143,7 +589,7 @@ int
 main(int argc, char** argv)
 {
     oasys::TclCommandInterp* interp;
-    oasys::ConsoleCommand console_cmd;
+    oasys::ConsoleCommand* console_cmd;
     std::string conf_file;
     bool conf_file_set = false;
     bool daemon = false;
@@ -168,10 +614,17 @@ main(int argc, char** argv)
         oasys::Getopt::usage("dtn-test");
         exit(1);
     }
-    
-    interp->reg(&console_cmd);
+
+    console_cmd = new oasys::ConsoleCommand();
+    interp->reg(console_cmd);
     interp->reg(new DTNOpenCommand());
     interp->reg(new DTNCloseCommand());
+    interp->reg(new DTNRegisterCommand());
+    interp->reg(new DTNUnregisterCommand());
+    interp->reg(new DTNBindCommand());
+    interp->reg(new DTNUnbindCommand());
+    interp->reg(new DTNSendCommand());
+    interp->reg(new DTNRecvCommand());
     interp->reg(new ShutdownCommand());
 
     if (conf_file_set) {
@@ -180,13 +633,13 @@ main(int argc, char** argv)
     
     log_notice("/dtn-test", "dtn-test starting up...");
     
-    if (console_cmd.port_ != 0) {
+    if (console_cmd->port_ != 0) {
         log_notice("/dtn-test", "starting console on %s:%d",
-                   intoa(console_cmd.addr_), console_cmd.port_);
-        interp->command_server("dtn-test", console_cmd.addr_, console_cmd.port_);
+                   intoa(console_cmd->addr_), console_cmd->port_);
+        interp->command_server("dtn-test", console_cmd->addr_, console_cmd->port_);
     }
 
-    if (daemon || (console_cmd.stdio_ == false)) {
+    if (daemon || (console_cmd->stdio_ == false)) {
         oasys::TclCommandInterp::instance()->event_loop();
     } else {
         oasys::TclCommandInterp::instance()->command_loop("dtn-test");
