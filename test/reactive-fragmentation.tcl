00f3e87019d10d33b3c67f37f434bd264e798ae0
demmer <devnull@localhost>
2006-08-01 18:34:44 -0700

    * fix the test to work with the new clayer interface

--- test/reactive-fragmentation.tcl
+++ test/reactive-fragmentation.tcl
@@ -4,7 +4,7 @@ net::num_nodes 2
 dtn::config
 dtn::config_interface tcp
 dtn::config_linear_topology ALWAYSON tcp true \
-	"test_write_delay=1000 sendbuf_len=512"
+	"test_write_delay=1000 block_length=512"
 
 test::script {
     puts "* Running dtnds"
@@ -13,35 +13,43 @@ test::script {
     puts "* Waiting for dtnds to start up"
     dtn::wait_for_dtnd *
 
-    dtn::tell_dtnd 0 tcl_registration dtn://host-0/test
+    dtn::tell_dtnd 1 tcl_registration dtn://host-1/test
 
     puts "* Waiting for links to open"
     dtn::wait_for_link_state 0 tcp-link:0-1 OPEN
     dtn::wait_for_link_state 1 tcp-link:1-0 OPEN
 
     puts "* Sending bundle"
-    set timestamp [dtn::tell_dtnd 1 sendbundle \
-	    dtn://host-1/test dtn://host-0/test length=8096]
+    set timestamp [dtn::tell_dtnd 0 sendbundle \
+	    dtn://host-0/test dtn://host-1/test length=8096]
 
     for {set i 0} {$i < 5} {incr i} {
-	set wait [expr int(5000 * rand())]
+	set wait [expr 2000 + int(2500 * rand())]
 	puts "* Waiting [expr $wait / 1000] seconds"
 	after $wait
 	
 	puts "* Interrupting the link"
-	dtn::tell_dtnd 1 link close tcp-link:1-0 
-	dtn::wait_for_link_state 1 tcp-link:1-0 UNAVAILABLE
-	dtn::tell_dtnd 1 link open tcp-link:1-0
-	dtn::wait_for_link_state 1 tcp-link:1-0 OPEN
+	dtn::tell_dtnd 0 link close tcp-link:0-1 
+	dtn::wait_for_link_state 0 tcp-link:0-1 UNAVAILABLE
+	dtn::tell_dtnd 0 link open tcp-link:0-1
+	dtn::wait_for_link_state 0 tcp-link:0-1 OPEN
     }
-	
+
     puts "* Waiting for the bundle to arrive and to be reassembled"
-    dtn::wait_for_bundle 0 "dtn://host-1/test,$timestamp" 30000
+    dtn::wait_for_bundle 1 "dtn://host-0/test,$timestamp" 30000
 
     puts "* Checking that no bundles are pending"
-    dtn::wait_for_bundle_stats 0 {0 pending 1 delivered}
-    dtn::wait_for_bundle_stats 1 {0 pending}
-    
+    dtn::wait_for_bundle_stats 0 {0 pending}
+    dtn::wait_for_bundle_stats 1 {0 pending 1 delivered}
+
+    puts "* Checking that it really did fragment"
+    set stats [dtn::tell_dtnd 1 bundle stats]
+    regexp {(\d+) received} $stats match received
+    if {$received == 1} {
+	error "only one bundle received"
+    }
+    puts "* $received fragments received"
+
     puts "* Test success!"
 }
 
