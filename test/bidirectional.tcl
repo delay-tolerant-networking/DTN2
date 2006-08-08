ad964b41900f6090fa524b95985e170693e47eb9
demmer <devnull@localhost>
2006-08-08 14:40:17 -0700

    * another iteration of the test that's more harsh on the killing front   but should also be more stable and have a chance of passing

--- test/bidirectional.tcl
+++ test/bidirectional.tcl
@@ -132,14 +132,16 @@ test::script {
     puts "* "
 
     puts "* Running dtnds"
-    dtn::run_dtnd *
+    for {set i 0} {$i < [net::num_nodes]} {incr i} {
+	set pids($i) [dtn::run_dtnd $i]
+    }
 
     puts "* Waiting for dtnds to start up"
     dtn::wait_for_dtnd *
     
     puts "* Running senders / receivers for $count bundles, sleep $sleep"
-    set rcvpid1 [dtn::run_app 0     dtnrecv "$eid1 -q -n $count"]
-    set rcvpid2 [dtn::run_app $last dtnrecv "$eid2 -q -n $count"]
+    set rcvpid1 [dtn::run_app 0     dtnrecv "$eid1 -q"]
+    set rcvpid2 [dtn::run_app $last dtnrecv "$eid2 -q"]
     set sndpid1 [dtn::run_app 0     dtnsend "-s $eid1 -d $eid2 -t d -z $sleep -n $count"]
     set sndpid2 [dtn::run_app $last dtnsend "-s $eid2 -d $eid1 -t d -z $sleep -n $count"]
 
@@ -150,26 +152,21 @@ test::script {
 	    break; # all done
 	}
 
-	set sleep [expr int(2000 * rand())]
+	set sleep [expr int(5000 * rand())]
 	puts "* Sleeping for $sleep msecs"
 	after $sleep
 	
 	puts "* Killing node 1"
-	dtn::stop_dtnd 1
+	tell::close_socket $net::host(1) [dtn::get_port console 1]
+	run::kill_pid 1 $pids(1) KILL	
 
-	set sleep [expr int(2000 * rand())]
+	set sleep [expr int(5000 * rand())]
 	puts "* Sleeping for $sleep msecs"
 	after $sleep
 
 	puts "* Starting node 1"
-	dtn::run_dtnd 1
+	set pids(1) [dtn::run_dtnd 1 ""]
 	dtn::wait_for_dtnd 1
-
-	puts "* Waiting for links to open"
-	dtn::wait_for_link_state 0 $cl-link:0-1 OPEN
-	dtn::wait_for_link_state 1 $cl-link:1-0 OPEN
-	dtn::wait_for_link_state 1 $cl-link:1-2 OPEN
-	dtn::wait_for_link_state 2 $cl-link:2-1 OPEN
     }
 
     puts "* Done with killing loop.. stats:"
@@ -177,26 +174,27 @@ test::script {
 	puts "Node $i: [tell_dtnd $i bundle stats]"
     }
 
-    puts "* Waiting for senders / receivers to complete"
+    puts "* Waiting for senders to complete"
     run::wait_for_pid_exit 0     $sndpid1
     run::wait_for_pid_exit $last $sndpid2
-    run::wait_for_pid_exit 0     $rcvpid1
-    run::wait_for_pid_exit $last $rcvpid2
+
+    puts "* Killing receivers"
+    run::kill_pid 0     $rcvpid1 TERM
+    run::kill_pid $last $rcvpid1 TERM
 
     # all we can really check for is that they all got transmitted,
     # since some might end up getting lost when the node gets killed,
     # but after the ack was sent
     foreach node [list 0 $last] {
 	puts "* Checking bundle stats on node $node"
-	dtn::check_bundle_stats $node \
-		$count "transmitted" \
+	dtn::check_bundle_stats $node $count "transmitted"
     }
-
-    puts "* Stopping all dtnds"
-    dtn::stop_dtnd *
 
     puts "* Test success!"
 }
 
 test::exit_script {
+    puts "* Stopping all dtnds"
+    dtn::stop_dtnd *
+
 }
