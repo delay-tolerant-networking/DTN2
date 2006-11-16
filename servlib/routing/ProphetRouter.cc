adb5049df46c06cd6ead74c9d0abc8a71289544d
demmer <devnull@localhost>
2006-11-16 10:53:47 -0800

    From Jeff Wilson   * Completely restructure initial ProphetRouter.cc (now months stale)     - delegate most functionality to ProphetController     - ProphetRouter serves as shim to deliver events into ProphetController

--- servlib/routing/ProphetRouter.cc
+++ servlib/routing/ProphetRouter.cc
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2006 Intel Corporation
+ *    Copyright 2006 Baylor University
  * 
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  */
 
 #include "BundleRouter.h"
-#include "RouteTable.h"
 #include "bundling/Bundle.h"
 #include "bundling/BundleActions.h"
 #include "bundling/BundleDaemon.h"
@@ -23,32 +22,98 @@
 #include "bundling/AnnounceBundle.h"
 #include "contacts/Contact.h"
 #include "contacts/ContactManager.h"
-#include "reg/Registration.h"
+#include <oasys/util/StringBuffer.h>
 #include <stdlib.h>
 
 #include "ProphetRouter.h"
 
 namespace dtn {
 
+ProphetParams ProphetRouter::params_;
+
 ProphetRouter::ProphetRouter()
-    : TableBasedRouter("ProphetRouter", "prophet")
+    : BundleRouter("ProphetRouter", "prophet")
 {
-    log_info("Initializing ProphetRouter");
+    log_info("ProphetRouter constructor");
+    // params_ default values are set by ProphetCommand
+}
+
+ProphetRouter::~ProphetRouter()
+{
+    oracle_->shutdown();
+    delete oracle_;
+}
+
+void
+ProphetRouter::initialize()
+{
+    // Call factory to instantiate ProphetController
+    ProphetController::init(&params_,pending_bundles_,actions_);
+
+    // For convenience, grab hold of that instance variable
+    oracle_ = ProphetController::instance();
+
+    // all done
+    log_info("ProphetRouter initialized");
+}
+
+void
+ProphetRouter::handle_event(BundleEvent* event)
+{
+    dispatch_event(event);
+}
+
+void
+ProphetRouter::get_routing_state(oasys::StringBuffer* buf)
+{
+    oracle_->dump_state(buf);
+}
+
+void
+ProphetRouter::handle_bundle_received(BundleReceivedEvent *event)
+{
+    Bundle* bundle = event->bundleref_.object();
+    oracle_->handle_bundle_received(bundle,event->contact_);
+}
+
+void
+ProphetRouter::handle_bundle_expired(BundleExpiredEvent *event)
+{
+    Bundle* bundle = event->bundleref_.object();
+    oracle_->handle_bundle_expired(bundle);
 }
 
 void
 ProphetRouter::handle_link_created(LinkCreatedEvent* event)
 {
+    // can't do anything with "only" a link, except to police this one ASSERTion
     ASSERT(event->link_->remote_eid().equals(EndpointID::NULL_EID()) == false);
-    TableBasedRouter::handle_link_created(event);
+}
+
+void
+ProphetRouter::handle_contact_up(ContactUpEvent* event)
+{
+    // ProphetController demux's which ProphetEncounter handles this contact
+    // (or creates a new one)
+    oracle_->new_neighbor(event->contact_);
 }
 
 void
 ProphetRouter::handle_contact_down(ContactDownEvent* event)
 {
-    route_table_->del_entries_for_nexthop(event->contact_->link());
+    // Let ProphetController know that the contact's off the wire
+    oracle_->neighbor_gone(event->contact_);
+}
 
-    TableBasedRouter::handle_contact_down(event);
+void
+ProphetRouter::handle_link_state_change_request(LinkStateChangeRequest* req)
+{
+    // Signals the appropriate ProphetEncounter instance to clear any
+    // pending outbound queues
+    if (req->old_state_ == Link::BUSY && req->state_ == Link::AVAILABLE)
+    {
+        oracle_->handle_link_state_change_request(req->contact_);
+    }
 }
 
 } // namespace dtn
--- servlib/routing/ProphetRouter.h
+++ servlib/routing/ProphetRouter.h
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2006 Intel Corporation
+ *    Copyright 2006 Baylor University
  * 
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,17 +17,72 @@
 #ifndef _PROPHET_ROUTER_H_
 #define _PROPHET_ROUTER_H_
 
-#include "TableBasedRouter.h"
+#include "ProphetController.h"
+#include "bundling/BundleList.h"
 #include "reg/AdminRegistration.h"
 
 namespace dtn {
 
-class ProphetRouter : public TableBasedRouter {
+class ProphetRouter : public BundleRouter {
 public:
     ProphetRouter();
+    virtual ~ProphetRouter();
 
+    /**
+     * post-constructor initialization routine
+     */
+    void initialize();
+
+    /**
+     * Event handler overridden from BundleRouter / BundleEventHandler
+     * that dispatches to the type specific handlers where
+     * appropriate.
+     */
+    void handle_event(BundleEvent*);
+
+    /**
+     * Dump the routing state.
+     */
+    void get_routing_state(oasys::StringBuffer*);
+
+    /**
+     * Handler for new bundle arrivals
+     */
+    void handle_bundle_received(BundleReceivedEvent*);
+
+    /**
+     * Handler for bundle end-of-life
+     */
+    void handle_bundle_expired(BundleExpiredEvent*);
+
+    /**
+     * Monitor new links, complain if EID is null
+     */
     void handle_link_created(LinkCreatedEvent* event);
-    void handle_contact_down(ContactDownEvent* event);
+
+    /**
+     * Prophet's "New Neighbor" signal, section 2.3, p. 13
+     */
+    void handle_contact_up(ContactUpEvent* );
+
+    /**
+     * Prophet's "Neighbor Gone" signal, section 2.3, p. 13
+     */
+    void handle_contact_down(ContactDownEvent* );
+
+    /**
+     * Clear pending outbound queues, if any
+     */
+    void handle_link_state_change_request(LinkStateChangeRequest*);
+
+    /**
+     * Prophet's configuration metrics that are propagated all the
+     * way down to ProphetController, ProphetEncounter, and ProphetNode
+     */
+    static ProphetParams      params_;
+
+protected:
+    ProphetController* oracle_;
 
 }; // ProphetRouter
 
