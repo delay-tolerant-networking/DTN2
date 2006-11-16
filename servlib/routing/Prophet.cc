fbd8c0681437ed21cef58e4e960ac4a7fefbabd3
demmer <devnull@localhost>
2006-11-16 10:54:23 -0800

    From Jeff Wilson   * instantiate singleton instance for ID generator   * define constant doubles to avoid linking problems (demmer)

---
+++ servlib/routing/Prophet.cc
@@ -0,0 +1,24 @@
+/*
+ *    Copyright 2006 Baylor University
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "Prophet.h"
+
+namespace dtn {
+Prophet::UniqueID* Prophet::UniqueID::instance_ = NULL;
+const double Prophet::DEFAULT_P_ENCOUNTER;
+const double Prophet::DEFAULT_BETA;
+const double Prophet::DEFAULT_GAMMA;
+} // dtn
