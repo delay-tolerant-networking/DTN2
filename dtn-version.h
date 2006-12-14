0d59227d6d9599d09ab480c1288f788fbc6a1cf3
demmer <devnull@localhost>
2006-12-13 18:03:04 -0800

    * update to the upcoming 2.3.0 release

--- dtn-version.h
+++ dtn-version.h
@@ -18,9 +18,9 @@
 #define _DTN_VERSION_H_
 
 #define DTN_VERSION_MAJOR	2
-#define DTN_VERSION_MINOR	2
+#define DTN_VERSION_MINOR	3
 #define DTN_VERSION_PATCH	0
-#define DTN_VERSION_STRING	"DTN Reference Implementation version 2.2.0"
+#define DTN_VERSION_STRING	"DTN Reference Implementation version 2.3.0"
 
 extern const char* dtn_version;
 
