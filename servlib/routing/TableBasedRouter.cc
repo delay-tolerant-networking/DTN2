b6775aad30e16e1e188f02f798cb177611a902d2
demmer <devnull@localhost>
2006-12-26 17:40:14 -0800

    * fix the handling of add_nexthop_routes so that it doesn't   erroneously add routes for a nexthop that's 'dtn:none'

--- servlib/routing/TableBasedRouter.cc
+++ servlib/routing/TableBasedRouter.cc
@@ -115,11 +115,12 @@ TableBasedRouter::handle_link_created(LinkCreatedEvent* event)
     // URI that doesn't have anything following the "hostname" part,
     // we add a wildcard of '/*' to match all service tags.
     
-    if (config_.add_nexthop_routes_) {
-        Link* link = event->link_;
-        EndpointID eid = link->remote_eid();
-        std::string eid_str = eid.str();
-        
+    Link* link = event->link_;
+    EndpointID eid = link->remote_eid();
+    std::string eid_str = eid.str();
+    
+    if (config_.add_nexthop_routes_ && eid != EndpointID::NULL_EID())
+    { 
         if (eid.scheme_str() == "dtn" &&
             eid.ssp().length() > 2 &&
             eid.ssp()[0] == '/' &&
