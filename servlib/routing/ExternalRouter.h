09289a3aea795e863f6dd64d4a92d8f88ccdc930
demmer <devnull@localhost>
2007-04-23 18:58:19 -0700

    * add a new AC_DEFINE to the configure script called HAVE_CONFIG_STATE   that's always set and allows testing for whether or not config.h was   properly included * for all header files that used to #include <config.h>, add a check   for the symbol to make the resulting error messages easier to   understand if a .cc file forgot to include config.h properly

--- servlib/routing/ExternalRouter.h
+++ servlib/routing/ExternalRouter.h
@@ -22,6 +22,10 @@
 #ifndef _EXTERNAL_ROUTER_H_
 #define _EXTERNAL_ROUTER_H_
 
+#ifndef HAVE_CONFIG_STATE
+#error "MUST INCLUDE config.h before including this file"
+#endif
+
 #if defined(XERCES_C_ENABLED) && defined(EXTERNAL_DP_ENABLED)
 
 #include "router-custom.h"
