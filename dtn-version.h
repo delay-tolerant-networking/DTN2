d61c4f244e671e129b481c3e62835b71e2f5ee92
demmer <devnull@localhost>
2007-06-15 13:25:54 -0700

    * update release notes and version markers in preparation for a 2.4.0   release

--- dtn-version.h
+++ dtn-version.h
@@ -2,9 +2,9 @@
 #define _DTN_VERSION_H_
 
 #define DTN_VERSION_MAJOR	2
-#define DTN_VERSION_MINOR	3
+#define DTN_VERSION_MINOR	4
 #define DTN_VERSION_PATCH	0
-#define DTN_VERSION_STRING	"2.3.0"
+#define DTN_VERSION_STRING	"2.4.0"
 
 extern const char* dtn_version;
 
