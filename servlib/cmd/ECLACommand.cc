c19f536045d8de89968e7f361171593a4a9862ec
jward <devnull@localhost>
2007-06-15 05:52:38 -0700

    - Added create_discovered_links and discovered_prev_hop_header commands - Slight change to copyright header

--- servlib/cmd/ECLACommand.cc
+++ servlib/cmd/ECLACommand.cc
@@ -1,19 +1,17 @@
-/*
-Copyright 2004-2006 BBN Technologies Corporation
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0
- 
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied.
-    
-See the License for the specific language governing permissions and limitations
-under the License.
-
-$Id$
-*/
+/* Copyright 2004-2006 BBN Technologies Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
@@ -48,6 +46,14 @@ ECLACommand::ECLACommand()
               &ExternalConvergenceLayer::server_port_,
               "ECLA server port",
               "The port to listen for external CLAs on") );
+    
+    bind_var( new oasys::BoolOpt("create_discovered_links",
+              &ExternalConvergenceLayer::create_discovered_links_,
+              "Whether external CLAs should create discovered links") );
+    
+    bind_var( new oasys::BoolOpt("discovered_prev_hop_header",
+              &ExternalConvergenceLayer::discovered_prev_hop_header_,
+              "Whether discovered links get prev-hop headers") );
 }
 
 int
--- servlib/cmd/ECLACommand.h
+++ servlib/cmd/ECLACommand.h
@@ -1,19 +1,17 @@
-/*
-Copyright 2004-2006 BBN Technologies Corporation
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0
- 
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied.
-    
-See the License for the specific language governing permissions and limitations
-under the License.
-
-$Id$
-*/
+/* Copyright 2004-2006 BBN Technologies Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #ifndef _ECLA_COMMAND_H_
 #define _ECLA_COMMAND_H_
