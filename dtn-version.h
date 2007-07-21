bf433e1bd8e5db7ebe3364b339a2c0ae414d19bc
demmer <devnull@localhost>
2007-07-20 17:15:31 -0700

    * bump the dtn version to 2.4.1

--- dtn-version.h
+++ dtn-version.h
@@ -19,8 +19,8 @@
 
 #define DTN_VERSION_MAJOR	2
 #define DTN_VERSION_MINOR	4
-#define DTN_VERSION_PATCH	0
-#define DTN_VERSION_STRING	"2.4.0"
+#define DTN_VERSION_PATCH	1
+#define DTN_VERSION_STRING	"2.4.1"
 
 extern const char* dtn_version;
 
