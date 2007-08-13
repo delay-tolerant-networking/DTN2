93e948e2872b612b2a6f6aa7399db259cbb2bf6d
demmer <devnull@localhost>
2007-08-13 16:29:28 -0700

    add a test for rerouting from a down link

--- test-utils/run-tcl-tests.tcl
+++ test-utils/run-tcl-tests.tcl
@@ -56,6 +56,7 @@ set tests(basic) {
     "ondemand-links.tcl"	""
     "no-duplicate-send.tcl"	""
     "reactive-fragmentation.tcl" ""
+    "reroute-from-down-link.tcl" ""
     "send-one-bundle.tcl"	"-cl tcp"
     "send-one-bundle.tcl"	"-cl tcp -length 0"
     "send-one-bundle.tcl"	"-cl tcp -length 5 -segment_length 1"
