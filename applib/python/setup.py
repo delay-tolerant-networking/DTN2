4f39b3075b8348b9da9979e33612bda48719c9d3
demmer <devnull@localhost>
2007-10-19 13:58:58 -0700

    * fix python build rules to support installation into a different   directory and to pass the version to the script

--- applib/python/setup.py
+++ applib/python/setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup, Extension
 from os import getenv
 
-INCDIR   = getenv('INCDIR')
-LIBDIR = getenv('LIBDIR')
+INCDIR  = getenv('INCDIR')
+LIBDIR  = getenv('LIBDIR')
+VERSION = getenv('VERSION')
 
-if INCDIR == None: raise ValueError('must set INCDIR')
-if LIBDIR == None: raise ValueError('must set LIBDIR')
+if INCDIR  == None: raise ValueError('must set INCDIR')
+if LIBDIR  == None: raise ValueError('must set LIBDIR')
+if VERSION == None: raise ValueError('must set VERSION')
 
 setup(name="dtnapi", 
-      version="2.4.1", 
+      version=VERSION,
       description="DTN API Python Bindings",
       author="Michael Demmer",
       author_email="demmer@cs.berkeley.edu",
@@ -17,5 +19,5 @@ setup(name="dtnapi",
       ext_modules=[Extension("_dtnapi", ["dtn_api_wrap_python.cc"],
                              include_dirs=[INCDIR],
 			     library_dirs=[LIBDIR],
-                             libraries=["dtnapi-2.4.1"])]
+                             libraries=["".join(['dtnapi-', VERSION])])]
       )
