6187e2cea4ce0339b87323db4b22ef8590159a1c
Jeff Wilson <jeff_wilson@baylor.edu>
2007-11-14 10:10:42 -0600

    * Expand test to cover more of BundleCore API

--- test/unit_tests/prophet-bundle-core-test.cc
+++ test/unit_tests/prophet-bundle-core-test.cc
@@ -3,17 +3,28 @@
 #endif
 #include <oasys/util/UnitTest.h>
 #include <oasys/thread/SpinLock.h>
+#include "conv_layers/NullConvergenceLayer.h"
+#include "contacts/Link.h"
+#include "routing/ProphetBundle.h"
+#include "routing/ProphetBundleList.h"
 #include "routing/ProphetBundleCore.h"
+#include "bundling/Bundle.h"
 #include "bundling/BundleActions.h"
 
 using namespace oasys;
 
+dtn::Bundle *b;
+dtn::BundleRef br("temp test ref");
+dtn::LinkRef l;
+dtn::NullConvergenceLayer *cl = NULL;
+dtn::BundleActions actions;
+oasys::SpinLock lock;
+dtn::ProphetBundleCore core(oasys::Builder::builder());
+
 DECLARE_TEST(ProphetBundleCore)
 {
-    dtn::BundleActions actions;
-    oasys::SpinLock* lock = new oasys::SpinLock();
-    dtn::ProphetBundleCore core("dtn://localhost",&actions,lock);
     prophet::LinkImpl link("dtn://remotehost");
+    dtn::ProphetBundleCore core("dtn://localhost",&actions,&lock);
     CHECK( core.is_route("dtn://localhost/prophet","dtn://localhost"));
     CHECK(!core.is_route("dtn://remotehost","dtn://localhost"));
     CHECK_EQUALSTR(core.prophet_id().c_str(), "dtn://localhost/prophet");
@@ -22,9 +33,91 @@ DECLARE_TEST(ProphetBundleCore)
     return UNIT_TEST_PASSED;
 }
 
+DECLARE_TEST(ProphetBundleWrapper)
+{
+    b = new dtn::Bundle(oasys::Builder::builder());
+    b->bundleid_ = 0;
+    b->payload_.init(0, dtn::BundlePayload::NODATA);
+    b->add_ref("test");
+    b->dest_.assign("dtn://dtn.howdy.dtn");
+    b->source_.assign("dtn://dtn.mysrc.dtn");
+    b->creation_ts_.seconds_ = 98765;
+    b->creation_ts_.seqno_ = 4321;
+    b->expiration_ = 123;
+    b->custody_requested_ = true;
+
+    br = b;
+    CHECK(br.object() != NULL);
+    dtn::ProphetBundle p(br);
+    CHECK_EQUALSTR(b->dest_.c_str(), p.destination_id().c_str());
+    CHECK_EQUALSTR(b->source_.c_str(), p.source_id().c_str());
+    CHECK_EQUAL(b->creation_ts_.seconds_, p.creation_ts());
+    CHECK_EQUAL(b->creation_ts_.seqno_, p.sequence_num());
+    CHECK_EQUAL(b->payload_.length(), p.size());
+    CHECK_EQUAL(p.num_forward(), 0);
+    CHECK(p.custody_requested());
+
+    return UNIT_TEST_PASSED;
+}
+
+DECLARE_TEST(ProphetBundleList)
+{
+    dtn::ProphetBundleList bl(&core);
+    CHECK(br.object() != NULL);
+    bl.add(br);
+    const prophet::Bundle* res = bl.find("dtn://dtn.howdy.dtn",
+                                         98765, 4321);
+    CHECK( res != NULL );
+
+    dtn::BundleRef bres("test finder temp");
+    bres = bl.find_ref(res);
+    CHECK( bres.object() != NULL );
+
+    bl.clear();
+    CHECK(bl.find("dtn://dtn.howdy.dtn",98765,4321) == NULL);
+
+    return UNIT_TEST_PASSED;
+}
+
+DECLARE_TEST(ProphetLinkWrapper)
+{
+    cl = new dtn::NullConvergenceLayer();;
+    l = dtn::Link::create_link("l", dtn::Link::OPPORTUNISTIC, cl,
+                               "ever-so-where-i-go", 0, NULL);
+    l->set_remote_eid(std::string("dtn://whodat.over.der.dtn"));
+    dtn::ProphetLink pl(l);
+    CHECK_EQUALSTR(l->nexthop(),pl.nexthop());
+    CHECK_EQUALSTR(l->remote_eid().c_str(),pl.remote_eid());
+
+    return UNIT_TEST_PASSED;
+}
+
+DECLARE_TEST(ProphetLinkList)
+{
+    dtn::ProphetLinkList pll;
+    pll.add(l);
+    CHECK( pll.find("dtn://whodat.over.der.dtn") != NULL );
+    dtn::LinkRef tmp("ProphetLinkList");
+    tmp = pll.find_ref("dtn://whodat.over.der.dtn");
+    CHECK( tmp.object() != NULL );
+    return UNIT_TEST_PASSED;
+}
+
+DECLARE_TEST(CleanErUpper)
+{
+    br = NULL;
+    cl->delete_link(l);
+    return UNIT_TEST_PASSED;
+}
+
 DECLARE_TESTER(ProphetBundleCore)
 {
     ADD_TEST(ProphetBundleCore);
+    ADD_TEST(ProphetBundleWrapper);
+    ADD_TEST(ProphetBundleList);
+    ADD_TEST(ProphetLinkWrapper);
+    ADD_TEST(ProphetLinkList);
+    ADD_TEST(CleanErUpper);
 }
 
 DECLARE_TEST_FILE(ProphetBundleCore, "prophet bundle core test");
