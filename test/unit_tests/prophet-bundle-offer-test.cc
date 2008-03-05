738bf5dc49b4cb0c197f44e72728f6b1c829e9de
Michael Demmer <demmer@cs.berkeley.edu>
2008-03-05 10:43:02 -0800

    update prophet tests to include dtn-config.h and fix build problems

--- test/unit_tests/prophet-bundle-offer-test.cc
+++ test/unit_tests/prophet-bundle-offer-test.cc
@@ -1,3 +1,4 @@
+#include <dtn-config.h>
 #include <oasys/util/UnitTest.h>
 
 #include "prophet/BundleImpl.h"
--- test/unit_tests/prophet-controller-test.cc
+++ test/unit_tests/prophet-controller-test.cc
@@ -1,3 +1,4 @@
+#include <dtn-config.h>
 #include <oasys/util/UnitTest.h>
 
 using namespace oasys;
