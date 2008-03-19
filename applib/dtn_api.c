745501a7d7be72eb63769b96f0e3ec3ca22b14e5
Michael Demmer <demmer@cs.berkeley.edu>
2008-03-19 13:45:38 -0700

    oops -- fix include path to be <limits.h>

--- applib/dtn_api.c
+++ applib/dtn_api.c
@@ -23,7 +23,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
-#include <sys/syslimits.h>
+#include <limits.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
