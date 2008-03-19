ed6b5bc805e6249d44d5ca24ca80364633467b26
Michael Demmer <demmer@cs.berkeley.edu>
2008-03-19 13:43:40 -0700

    fix a long-standing limitation of the RPC interface to handle apps that ask for DTN_PAYLOAD_MEM but then fail to be able to receive a big (>50K) bundle by transferring the data over the API in files and then reading it into memory on behalf of the app

--- applib/APIServer.cc
+++ applib/APIServer.cc
@@ -1224,14 +1224,18 @@ APIClient::handle_recv()
         spec.metadata.metadata_val = (dtn_extension_block_t *)buf;
     }
 
-    // XXX/demmer verify bundle size constraints
-    payload.location = location;
-    
+    size_t payload_len = b->payload().length();
+
+    if (location == DTN_PAYLOAD_MEM && payload_len > DTN_MAX_BUNDLE_MEM)
+    {
+        log_debug("app requested memory delivery but payload is too big (%zu bytes)... "
+                  "using files instead",
+                  payload_len);
+        location = DTN_PAYLOAD_FILE;
+    }
+
     if (location == DTN_PAYLOAD_MEM) {
         // the app wants the payload in memory
-        // XXX/demmer verify bounds
-
-        size_t payload_len = b->payload().length();
         payload.buf.buf_len = payload_len;
         if (payload_len != 0) {
             buf.reserve(payload_len);
@@ -1243,9 +1247,7 @@ APIClient::handle_recv()
         
     } else if (location == DTN_PAYLOAD_FILE) {
         char *tdir, templ[64];
-
-        // XXX/demmer do this with a hard link
-
+        
         tdir = getenv("TMP");
         if (tdir == NULL) {
             tdir = getenv("TEMP");
@@ -1253,7 +1255,7 @@ APIClient::handle_recv()
         if (tdir == NULL) {
             tdir = "/tmp";
         }
-
+        
         snprintf(templ, sizeof(templ), "%s/bundlePayload_XXXXXX", tdir);
 
         if (tmpfile.mkstemp(templ) == -1) {
@@ -1266,13 +1268,7 @@ APIClient::handle_recv()
                      strerror(errno));
         }
         
-        if (b->payload().location() == BundlePayload::MEMORY) {
-            tmpfile.writeall((char*)b->payload().memory_buf()->buf(),
-                             b->payload().length());
-            
-        } else {
-            b->payload().copy_file(&tmpfile);
-        }
+        b->payload().copy_file(&tmpfile);
 
         payload.filename.filename_val = (char*)tmpfile.path();
         payload.filename.filename_len = tmpfile.path_len() + 1;
