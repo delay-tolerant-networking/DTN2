aa68015ae29c7715f26f6f5ccbbde82406f65e58
Michael Demmer <demmer@cs.berkeley.edu>
2008-06-11 12:32:48 -0400

    add a test to handle the case where a sender node doesn't enable fragmentation but the receiver side does

--- test/reactive-fragmentation.tcl
+++ test/reactive-fragmentation.tcl
@@ -37,7 +37,45 @@ test::script {
 
     testlog "Sending bundle"
     set timestamp [dtn::tell_dtnd 0 sendbundle \
-	    dtn://host-0/test dtn://host-1/test length=8096]
+	    dtn://host-0/test dtn://host-1/test length=8096 expiration=3600]
+
+    for {set i 0} {$i < 5} {incr i} {
+	set wait [expr 2000 + int(2500 * rand())]
+	testlog "Waiting [expr $wait / 1000] seconds"
+	after $wait
+	
+	testlog "Interrupting the link"
+	dtn::tell_dtnd 0 link close tcp-link:0-1 
+	dtn::wait_for_link_state 0 tcp-link:0-1 UNAVAILABLE
+	dtn::tell_dtnd 0 link open tcp-link:0-1
+	dtn::wait_for_link_state 0 tcp-link:0-1 OPEN
+    }
+
+    testlog "Waiting for the bundle to arrive and to be reassembled"
+    dtn::wait_for_bundle 1 "dtn://host-0/test,$timestamp" 30
+
+    testlog "Checking that no bundles are pending"
+    dtn::wait_for_bundle_stats 0 {0 pending}
+    dtn::wait_for_bundle_stats 1 {0 pending 1 delivered}
+
+    testlog "Checking that it really did fragment"
+    set stats [dtn::tell_dtnd 1 bundle stats]
+    regexp {(\d+) received} $stats match received
+    if {$received == 1} {
+	error "only one bundle received"
+    }
+    testlog "$received fragments received"
+
+    testlog "Resetting stats"
+    tell_dtnd 0 bundle reset_stats
+    tell_dtnd 1 bundle reset_stats
+
+    testlog "Disabling reactive fragmentation on node 1"
+    tell_dtnd 0 link reconfigure tcp-link:0-1 reactive_frag_enabled=0
+
+    testlog "Sending another bundle"
+    set timestamp [dtn::tell_dtnd 0 sendbundle \
+	    dtn://host-0/test dtn://host-1/test length=8096 expiration=3600]
 
     for {set i 0} {$i < 5} {incr i} {
 	set wait [expr 2000 + int(2500 * rand())]
